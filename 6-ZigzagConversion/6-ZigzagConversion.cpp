f792122d349af0f15a09ac0df714591db5e90e7f
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 15:03:23 +0530

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.cpp
@@ -0,0 +1,26 @@
+// Last updated: 3/18/2026, 3:03:21 PM
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+
+        vector<string> rows(numRows);
+        int row = 0;
+        int dir = 1;   // 1 = down, -1 = up
+
+        for (char c : s) {
+            rows[row] += c;
+
+            if (row == 0) dir = 1;
+            if (row == numRows - 1) dir = -1;
+
+            row += dir;
+        }
+
+        string ans = "";
+        for (string r : rows)
+            ans += r;
+
+        return ans;
+    }
+};
