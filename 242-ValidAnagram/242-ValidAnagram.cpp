e5f7a39bc1f406d06f67390a41e9e7a0b76bb3a7
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:28:33 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,9 @@
+// Last updated: 3/18/2026, 2:28:31 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
