6b56233fcebd36679f1a5db7ac61fc26a75311c3
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 15:02:44 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.cpp
@@ -0,0 +1,24 @@
+// Last updated: 3/18/2026, 3:02:41 PM
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int l = 0;
+        int r = n-1;
+
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }  
+      return l;
+    }
+};
