ea75d05b28853e39951123f4e9aa4762a60a5ab6
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:20:59 +0530

    Create 3653-MaximumSubarraySumWithLengthDivisibleByK

---
+++ 3653-MaximumSubarraySumWithLengthDivisibleByK/3653-MaximumSubarraySumWithLengthDivisibleByK.cpp
@@ -0,0 +1,20 @@
+// Last updated: 3/18/2026, 2:20:57 PM
+using ll = long long;
+
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {       
+        ll prefSum = 0;
+        ll subMax = LLONG_MIN;
+        vector<ll> minSoFar(k, LLONG_MAX / 2);
+        minSoFar[(k - 1) % k] = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefSum += nums[i];
+            subMax = max(subMax, prefSum - minSoFar[i % k]);
+            minSoFar[i % k] = min(minSoFar[i % k], prefSum);
+        }
+
+        return subMax;
+    }
+};
