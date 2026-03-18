561c45ba27600cbe695534f72dca70c29648c2aa
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:23:58 +0530

    Create 1800-ConcatenationOfConsecutiveBinaryNumbers

---
+++ 1800-ConcatenationOfConsecutiveBinaryNumbers/1800-ConcatenationOfConsecutiveBinaryNumbers.cpp
@@ -0,0 +1,14 @@
+// Last updated: 3/18/2026, 2:23:56 PM
+class Solution {
+public:
+    static int concatenatedBinary(int n) {
+        constexpr int mod=1e9+7;
+        uint64_t ans=0;
+        for(unsigned x=1; x<=n; x++){
+            const int bw=32-countl_zero(x);
+            ans=(ans<<bw)+x;
+            if (ans>=mod) ans%=mod;
+        }
+        return ans;
+    }
+};
