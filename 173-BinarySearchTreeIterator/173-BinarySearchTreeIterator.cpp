0506a3f8e952497ebd27802bf98d82e906f5ef71
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:29:36 +0530

    Create 173-BinarySearchTreeIterator

---
+++ 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.cpp
@@ -0,0 +1,27 @@
+// Last updated: 3/18/2026, 2:29:33 PM
+class BSTIterator {
+    stack<TreeNode*> st;
+
+    void pushLeft(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushLeft(root);
+    }
+    
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+        pushLeft(node->right);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+};
