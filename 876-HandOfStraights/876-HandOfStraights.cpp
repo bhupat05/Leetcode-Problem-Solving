25f180dbf23c70ffc5de658514d8d1b7162b5c42
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:26:08 +0530

    Create 876-HandOfStraights

---
+++ 876-HandOfStraights/876-HandOfStraights.cpp
@@ -0,0 +1,32 @@
+// Last updated: 3/18/2026, 2:26:06 PM
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+
+        if(n % groupSize) {
+            return false;
+        }
+
+        map<int, int> mp;
+
+        for(int &handNumber : hand){
+            mp[handNumber]++;
+        }
+
+        while(!mp.empty()){
+            int curr = mp.begin() -> first;
+
+            for(int i = 0; i < groupSize; i++){
+                if(mp[curr + i] == 0) {
+                    return false;
+                }
+                mp[curr + i]--;
+                if(mp[curr + i] < 1) {
+                    mp.erase(curr + i);
+                }
+            }
+        }
+        return true;
+    }
+};
