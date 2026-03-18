2edb855830e2fac4f00d32671dcb2197f9573e67
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:28:05 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.cpp
@@ -0,0 +1,14 @@
+// Last updated: 3/18/2026, 2:28:03 PM
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> t(n + 1);
+
+        t[0] = 0;
+
+        for(int i = 0; i <=n; i++){
+            t[i] = t[i/2] + i % 2;
+        }
+        return t;
+    }
+};
