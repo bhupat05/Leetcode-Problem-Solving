5b278b7a8ba70e864b83b3be701d32510c202d23
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 15:02:21 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,14 @@
+// Last updated: 3/18/2026, 3:02:19 PM
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i > maxi) return false;
+            maxi = max(maxi, i + nums[i]);
+        }
+        return true;
+    }
+};
