44670d2a3a216a0d56e9c611c838035a6f7ca518
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:24:50 +0530

    Create 1448-Maximum69Number

---
+++ 1448-Maximum69Number/1448-Maximum69Number.cpp
@@ -0,0 +1,19 @@
+// Last updated: 3/18/2026, 2:24:48 PM
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '6') {
+                s[i] = '9';
+                break;
+            }
+        }
+
+        return stoi(s);
+    }
+};
