9c3ada58e97c5da6e8d14b41bcc4549d89ed101d
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:23:54 +0530

    Create 1845-LargestSubmatrixWithRearrangements

---
+++ 1845-LargestSubmatrixWithRearrangements/1845-LargestSubmatrixWithRearrangements.cpp
@@ -0,0 +1,30 @@
+// Last updated: 3/18/2026, 2:23:52 PM
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 1) {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < m; i++) {
+            vector<int> row = matrix[i];
+
+            sort(row.begin(), row.end(), greater<int>());
+
+            for(int j = 0; j < n; j++) {
+                ans = max(ans, row[j] * (j + 1));
+            }
+        }
+        return ans;
+    }
+};
