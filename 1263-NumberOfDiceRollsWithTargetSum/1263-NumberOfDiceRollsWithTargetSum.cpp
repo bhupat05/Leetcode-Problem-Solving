fefb596fb6db6d9b1de7a61ac323cafe8fafd037
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:25:19 +0530

    Create 1263-NumberOfDiceRollsWithTargetSum

---
+++ 1263-NumberOfDiceRollsWithTargetSum/1263-NumberOfDiceRollsWithTargetSum.cpp
@@ -0,0 +1,21 @@
+// Last updated: 3/18/2026, 2:25:17 PM
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        const int MOD = 1e9 + 7;
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        dp[0][0] = 1;
+
+        for (int dice = 1; dice <= n; ++dice) {
+            for (int t = 1; t <= target; ++t) {
+                for (int face = 1; face <= k; ++face) {
+                    if (t - face >= 0) {
+                        dp[dice][t] = (dp[dice][t] + dp[dice - 1][t - face]) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+};
