835a475bd41dac4e0c88ca06da4d2a8b08f6794d
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 15:01:56 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.cpp
@@ -0,0 +1,24 @@
+// Last updated: 3/18/2026, 3:01:54 PM
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int start = 0;
+        int end = m * n - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(matrix[mid / n][mid % n] > target){
+                end = mid - 1;
+            }else if(matrix[mid / n][mid % n] < target){
+                start = mid + 1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
