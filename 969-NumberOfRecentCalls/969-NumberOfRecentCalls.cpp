4bf067ec6a8acbebca5c0ebb68c1ff3c46206ef2
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:25:55 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.cpp
@@ -0,0 +1,24 @@
+// Last updated: 3/18/2026, 2:25:53 PM
+class RecentCounter {
+public:
+    queue<int> q;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+
+        while(q.front() < t - 3000) {
+            q.pop();
+        }
+
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
