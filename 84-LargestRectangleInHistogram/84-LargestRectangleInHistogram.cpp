d99ba10f788ffb70f31ec57cc7ea77cbd16c0a0d
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 15:01:43 +0530

    Create 84-LargestRectangleInHistogram

---
+++ 84-LargestRectangleInHistogram/84-LargestRectangleInHistogram.cpp
@@ -0,0 +1,37 @@
+// Last updated: 3/18/2026, 3:01:41 PM
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+
+        // Find next smaller element to the right
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        // Find previous smaller element to the left
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            ans = max(ans, heights[i] * width);
+        }
+
+        return ans;
+    }
+};
