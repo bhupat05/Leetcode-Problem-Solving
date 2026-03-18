30387b64dbccb8e014a6941551a8a6ddf4f2f79d
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:28:12 +0530

    Create 322-CoinChange

---
+++ 322-CoinChange/322-CoinChange.cpp
@@ -0,0 +1,17 @@
+// Last updated: 3/18/2026, 2:28:10 PM
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        const int INF = 1e9;
+        vector<int> dp(amount + 1, INF);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] = min(dp[j], 1 + dp[j - coin]);
+            }
+        }
+
+        return dp[amount] == INF ? -1 : dp[amount];
+    }
+};
