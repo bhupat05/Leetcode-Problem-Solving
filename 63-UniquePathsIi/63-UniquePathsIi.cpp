8516745d5bf402c65b9143376790c235a19371e4
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 15:02:12 +0530

    Create 63-UniquePathsIi

---
+++ 63-UniquePathsIi/63-UniquePathsIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 3/18/2026, 3:02:10 PM
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> dp(n, 0);
+        dp[0] = (grid[0][0] == 0);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dp[j] = 0;  
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
