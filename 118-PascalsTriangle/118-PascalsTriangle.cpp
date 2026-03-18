92a01ccb5c0f9b0240ebe2b69962c636caae1e3b
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:30:31 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,15 @@
+// Last updated: 3/18/2026, 2:30:29 PM
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result(numRows);
+
+        for(int i = 0; i < numRows; i++){
+            result[i] = vector<int>(i + 1, 1);
+            for(int j = 1; j < i; j++){
+                result[i][j] = result[i - 1][j] + result[i - 1][j - 1];
+            }
+        }
+        return result;
+    }
+};
