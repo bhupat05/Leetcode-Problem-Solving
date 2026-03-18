cdc4db4f70f935052e3f21ba42ef1d7410951915
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:30:20 +0530

    Create 127-WordLadder

---
+++ 127-WordLadder/127-WordLadder.cpp
@@ -0,0 +1,36 @@
+// Last updated: 3/18/2026, 2:30:18 PM
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        st.erase(beginWord);
+
+        while(!q.empty()) {
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            if(word == endWord) return steps;
+
+            for(int i = 0; i < word.size(); i++) {
+                char original = word[i];
+
+                for(char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+
+                    if(st.count(word)) {
+                        st.erase(word);
+                        q.push({word, steps + 1});
+                    }
+                }
+
+                word[i] = original;
+            }
+
+        }
+        return 0;
+
+    }
+};
