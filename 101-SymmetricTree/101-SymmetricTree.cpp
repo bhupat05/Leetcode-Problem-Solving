3c31fbed2a18761eedd0eee3199a9342cf6488e5
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:30:53 +0530

    Create 101-SymmetricTree

---
+++ 101-SymmetricTree/101-SymmetricTree.cpp
@@ -0,0 +1,17 @@
+// Last updated: 3/18/2026, 2:30:51 PM
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if(left == NULL && right == NULL) return true;
+        if(left == NULL || right == NULL) return false;
+        if(left->val != right->val) return false;
+
+        return isMirror(left->left, right->right) &&
+               isMirror(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return isMirror(root->left, root->right);
+    }
+};
