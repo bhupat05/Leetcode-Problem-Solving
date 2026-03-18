5ba0b70ac9470f0589e92329b7cfabbaf53e0a79
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:24:03 +0530

    Create 1732-MinimumOneBitOperationsToMakeIntegersZero

---
+++ 1732-MinimumOneBitOperationsToMakeIntegersZero/1732-MinimumOneBitOperationsToMakeIntegersZero.cpp
@@ -0,0 +1,12 @@
+// Last updated: 3/18/2026, 2:24:01 PM
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+};
