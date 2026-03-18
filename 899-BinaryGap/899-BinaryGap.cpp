eeabd8d71cfe7acdd0d176dc39bb5076b2d58784
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:26:04 +0530

    Create 899-BinaryGap

---
+++ 899-BinaryGap/899-BinaryGap.cpp
@@ -0,0 +1,22 @@
+// Last updated: 3/18/2026, 2:26:02 PM
+class Solution {
+public:
+    int binaryGap(int n) {
+        int last = -1;   
+        int ans = 0;
+        int pos = 0;     
+        
+        while (n > 0) {
+            if (n & 1) {  
+                if (last != -1) {
+                    ans = max(ans, pos - last);
+                }
+                last = pos;
+            }
+            n >>= 1;
+            pos++;
+        }
+        
+        return ans;
+    }
+};
