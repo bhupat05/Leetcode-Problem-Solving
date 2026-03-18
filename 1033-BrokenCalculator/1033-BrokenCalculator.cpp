880f1304782db7fffa6e8a29d1aea28470d4a5c7
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-18 14:25:40 +0530

    Create 1033-BrokenCalculator

---
+++ 1033-BrokenCalculator/1033-BrokenCalculator.cpp
@@ -0,0 +1,17 @@
+// Last updated: 3/18/2026, 2:25:38 PM
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count = 0;
+
+        while (target > startValue) {
+            if (target % 2 == 0)
+                target /= 2;
+            else
+                target += 1;
+            count++;
+        }
+
+        return count + (startValue - target);
+    }
+};
