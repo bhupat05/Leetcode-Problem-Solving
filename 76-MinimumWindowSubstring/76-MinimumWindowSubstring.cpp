2d1c4bb8e5dd7341a4696fcb8bf2a021e69838af
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-26 01:23:59 +0530

    File updated at 3/26/2026, 1:23:59 AM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
@@ -1,35 +1,27 @@
-// Last updated: 3/18/2026, 3:01:51 PM
-class Solution {
-public:
-    string minWindow(string s, string t) {
-        int m = s.size(), n = t.size();
-        if (n > m) return "";
-
-        vector<int> v(128, 0);
-
-        for (char c : t) v[c]++;
-
-        int req = n;
-        int l = 0;
-        int minLen = INT_MAX;
-        int start = 0;
-
-        for (int i = 0; i < m; i++) {
-            v[s[i]]--;
-            if (v[s[i]] >= 0) req--;
-
-            while (req == 0) {
-                if (i - l + 1 < minLen) {
-                    minLen = i - l + 1;
-                    start = l;
-                }
-
-                v[s[l]]++;
-                if (v[s[l]] > 0) req++;
-                l++;
-            }
-        }
-
-        return minLen == INT_MAX ? "" : s.substr(start, minLen);
-    }
-};
+// Last updated: 3/26/2026, 1:23:59 AM
+1class Solution {
+2public:
+3    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+4        int i = 0;
+5        vector<vector<int>> result;
+6        int n = intervals.size();
+7
+8        while(i < n){
+9            if(intervals[i][1] < newInterval[0]){
+10                result.push_back(intervals[i]);
+11            }else if(intervals[i][0] > newInterval[1]){
+12                break;
+13            }else{
+14                newInterval[0] = min(newInterval[0], intervals[i][0]);
+15                newInterval[1] = max(newInterval[1], intervals[i][1]);
+16            }
+17            i++;
+18        }
+19        result.push_back(newInterval);
+20        while(i < n){
+21            result.push_back(intervals[i]);
+22            i++;
+23        }
+24        return result;
+25    }
+26};
