2129d8df65868bdee3d7d62e0ffb3cd1096eafbf
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-26 16:32:56 +0530

    File updated at 3/26/2026, 4:32:56 PM

--- 71-SimplifyPath/71-SimplifyPath.cpp
+++ 71-SimplifyPath/71-SimplifyPath.cpp
@@ -1,33 +1,36 @@
-// Last updated: 3/18/2026, 3:01:59 PM
-class Solution {
-public:
-    string simplifyPath(string path) {
-        stack<string> st;
-        string curr = "";
-
-        for (int i = 0; i <= path.size(); i++) {
-            if (i == path.size() || path[i] == '/') {
-                if (curr == "" || curr == ".") {
-                } 
-                else if (curr == "..") {
-                    if (!st.empty()) st.pop();
-                } 
-                else {
-                    st.push(curr);
-                }
-                curr = "";   
-            } 
-            else {
-                curr += path[i];
-            }
-        }
-
-        string result = "";
-        while (!st.empty()) {
-            result = "/" + st.top() + result;
-            st.pop();
-        }
-
-        return result.empty() ? "/" : result;
-    }
-};
+// Last updated: 3/26/2026, 4:32:56 PM
+1class Solution {
+2public:
+3    string simplifyPath(string path) {
+4        stack<string> st;
+5        string curr = "";
+6
+7        for(int i = 0; i <= path.size(); i++) {
+8            if(i == path.size() || path[i] == '/') {
+9                
+10                if(curr == "" || curr == ".") {
+11                    // do nothing
+12                } 
+13                else if(curr == "..") {
+14                    if(!st.empty()) st.pop();
+15                } 
+16                else {
+17                    st.push(curr);
+18                }
+19
+20                curr = "";
+21            } 
+22            else {
+23                curr += path[i];
+24            }
+25        }
+26
+27        string res = "";
+28        while(!st.empty()) {
+29            res = "/" + st.top() + res;
+30            st.pop();
+31        }
+32
+33        return res.empty() ? "/" : res;
+34    }
+35};
