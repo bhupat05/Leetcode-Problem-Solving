c624c8815d3f7ba66eb33bd3aa2c59bf69df31e4
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-03-30 16:24:32 +0530

    File updated at 3/30/2026, 4:24:32 PM

--- 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.cpp
+++ 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.cpp
@@ -1,27 +1,24 @@
-// Last updated: 3/18/2026, 2:29:33 PM
-class BSTIterator {
-    stack<TreeNode*> st;
-
-    void pushLeft(TreeNode* node) {
-        while (node) {
-            st.push(node);
-            node = node->left;
-        }
-    }
-
-public:
-    BSTIterator(TreeNode* root) {
-        pushLeft(root);
-    }
-    
-    int next() {
-        TreeNode* node = st.top();
-        st.pop();
-        pushLeft(node->right);
-        return node->val;
-    }
-    
-    bool hasNext() {
-        return !st.empty();
-    }
-};
+// Last updated: 3/30/2026, 4:24:32 PM
+1class Solution {
+2public:
+3    int lh(TreeNode* root) {
+4        if(!root) return 0;
+5        return 1 + lh(root->left);
+6    }
+7
+8    int rh(TreeNode* root) {
+9        if(!root) return 0;
+10        return 1 + rh(root->right);
+11    }
+12
+13    int countNodes(TreeNode* root) {
+14        if(!root) return 0;
+15
+16        int l = lh(root);
+17        int r = rh(root);
+18
+19        if(l == r) return (1 << l) - 1;
+20
+21        return 1 + countNodes(root->left) + countNodes(root->right);
+22    }
+23};
