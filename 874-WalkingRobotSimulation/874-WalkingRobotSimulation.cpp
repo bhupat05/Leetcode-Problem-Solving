a6e1c105ff4c7a5d045e686956dce91f109e1418
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-04-07 09:01:10 +0530

    File updated at 4/7/2026, 9:01:09 AM

--- 874-WalkingRobotSimulation/874-WalkingRobotSimulation.cpp
+++ 874-WalkingRobotSimulation/874-WalkingRobotSimulation.cpp
@@ -1,48 +1,86 @@
-// Last updated: 4/6/2026, 9:15:47 AM
-1class Solution {
+// Last updated: 4/7/2026, 9:01:09 AM
+1class Robot {
 2public:
-3    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-4        int n = commands.size();   
-5        set<pair<int, int>> st;
-6
-7        for (auto &v : obstacles) {
-8            st.insert({v[0], v[1]});
-9        }
-10        
-11
-12        int x = 0;
-13        int y = 0;
-14        int dis = 0;
-15        int d= 0;
-16
-17        for(int it : commands) {
-18            if(it == -1) {
-19                d = (d + 1) % 4;
-20            } else if(it == -2) {
-21                d = (d + 3) % 4;
-22            } else {
-23                if(d == 0) {
-24                    while(it-- && !st.count({x, y + 1})) {
-25                        y++;
-26                    }
-27                } else if(d == 1) {
-28                    while(it-- && !st.count({x + 1, y})) {
-29                        x++;
-30                    }
-31                } else if(d == 2) {
-32                    while(it-- && !st.count({x, y - 1})) {
-33                        y--;
-34                    }
-35                } else {
-36                     while(it-- && !st.count({x - 1, y})) {
-37                        x--;
-38                    }
-39                }
-40                
-41            }   
-42            dis = max(dis, (x * x + y * y));
-43
-44        }
-45        return dis;
-46    }
-47};
+3    int x = 0;
+4    int y = 0;
+5    int d = 1;
+6    int w;
+7    int h;
+8
+9    Robot(int width, int height) {
+10        w = width;
+11        h = height;
+12    }
+13    
+14    void step(int n) {
+15        int mod = 2 * (w + h - 2);   
+16        n %= mod;     
+17         if(n == 0) n = mod;               
+18
+19        while(n > 0) {
+20            if(d == 1) {
+21                int dis = w - x - 1;
+22                if(dis >= n) {
+23                    x += n;
+24                    n = 0;
+25                } else {
+26                    x += dis;
+27                    n -= dis;
+28                    d = (d + 3) % 4;
+29                }
+30            } 
+31            else if(d == 0) {
+32                int dis = h - y - 1;
+33                if(dis >= n) {
+34                    y += n;
+35                    n = 0;
+36                } else {
+37                    y += dis;
+38                    n -= dis;
+39                    d = (d + 3) % 4;
+40                }
+41            } 
+42            else if(d == 3) {
+43                int dis = x;
+44                if(dis >= n) {
+45                    x -= n;
+46                    n = 0;
+47                } else {
+48                    x -= dis;
+49                    n -= dis;
+50                    d = (d + 3) % 4;
+51                }
+52            } 
+53            else {
+54                int dis = y;
+55                if(dis >= n) {
+56                    y -= n;
+57                    n = 0;
+58                } else {
+59                    y -= dis;
+60                    n -= dis;
+61                    d = (d + 3) % 4;
+62                }
+63            }
+64        }
+65    }
+66    
+67    vector<int> getPos() {
+68        return {x, y};
+69    }
+70    
+71    string getDir() {
+72        if(d == 0) return "North";
+73        else if(d == 1) return "East";
+74        else if(d == 2) return "South";
+75        else return "West";
+76    }
+77};
+78
+79/**
+80 * Your Robot object will be instantiated and called as such:
+81 * Robot* obj = new Robot(width, height);
+82 * obj->step(num);
+83 * vector<int> param_2 = obj->getPos();
+84 * string param_3 = obj->getDir();
+85 */
