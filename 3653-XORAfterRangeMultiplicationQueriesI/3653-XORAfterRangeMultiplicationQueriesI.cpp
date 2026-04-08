9a7500ac13b10154474ae702768a10f74b8097e4
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-04-08 09:43:23 +0530

    File updated at 4/8/2026, 9:43:22 AM

--- 3653-XORAfterRangeMultiplicationQueriesI/3653-XORAfterRangeMultiplicationQueriesI.cpp
+++ 3653-XORAfterRangeMultiplicationQueriesI/3653-XORAfterRangeMultiplicationQueriesI.cpp
@@ -1,27 +1,20 @@
-// Last updated: 4/8/2026, 9:38:43 AM
+// Last updated: 4/8/2026, 9:43:22 AM
 1class Solution {
 2public:
 3    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-4        int n = nums.size();
-5        int q = queries.size();
-6        int mod = 1e9 + 7;
-7
-8        for(auto it : queries) {
-9            int idx = it[0];
-10            int r = it[1];
-11            int k = it[2];
-12            int v = it[3];
+4        const int MOD = 1e9 + 7;
+5
+6        for (const auto& q : queries) {
+7            int l = q[0], r = q[1], k = q[2], v = q[3];
+8
+9            for (int idx = l; idx <= r; idx += k) {
+10                nums[idx] = (1LL * nums[idx] * v) % MOD;
+11            }
+12        }
 13
-14            while(idx <= r) {
-15                nums[idx] = (1LL * nums[idx] * v) % mod;
-16                idx += k;
-17            }
-18        }
-19
-20        int x = nums[0];
-21        for(int i = 1; i < n; i++) {
-22            x ^= nums[i];
-23        }
-24        return x;
-25    }
-26};
+14        int result = 0;
+15        for (int val : nums) result ^= val;
+16
+17        return result;
+18    }
+19};
