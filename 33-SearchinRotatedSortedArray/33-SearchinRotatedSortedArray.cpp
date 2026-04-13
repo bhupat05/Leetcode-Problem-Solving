823320ee83857564a446f2c16b174a121902f4fa
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-04-13 11:07:15 +0530

    File updated at 4/13/2026, 11:07:15 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,24 +1,22 @@
-// Last updated: 4/13/2026, 10:55:34 AM
+// Last updated: 4/13/2026, 11:07:15 AM
 1class Solution {
 2public:
-3    int findMin(vector<int>& nums) {
+3    bool isPerfectSquare(int num) {
 4        int l = 0;
-5        int r = nums.size() - 1;
-6
-7        while(l < r) {
-8            int mid = l + (r - l) / 2;
-9
-10            if(nums[l] == nums[mid] && nums[mid] == nums[r]) {
-11                l++;
-12                r--;
-13            }
-14            else if(nums[mid] > nums[r]) {
+5        int r = num;
+6        int result = 0;
+7
+8        while(l <= r) {
+9            int mid = l + (r - l) / 2;
+10
+11            if((long long)mid * mid > num) {
+12                r = mid - 1;
+13            } else {
+14                result = mid;
 15                l = mid + 1;
-16            } 
-17            else {
-18                r = mid;
-19            }
-20        }
-21        return nums[l];  
-22    }
-23};
+16            }
+17        }
+18        return result * result == num;
+19        
+20    }
+21};
