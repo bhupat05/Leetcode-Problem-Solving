943748a1f8093958bd82f0dcf9184449cf9dbf18
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-05-04 08:23:33 +0530

    File updated at 5/4/2026, 8:23:33 AM

--- 48-RotateImage/48-RotateImage.cpp
+++ 48-RotateImage/48-RotateImage.cpp
@@ -1,18 +1,18 @@
-// Last updated: 3/18/2026, 3:02:28 PM
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = m;
-
-        for(int i = 0; i < m; i++){
-            for(int j = i; j < n; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-
-        for(int i = 0; i < m; i++){
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-    }
-};
+// Last updated: 5/4/2026, 8:23:33 AM
+1class Solution {
+2public:
+3    void rotate(vector<vector<int>>& matrix) {
+4        int m = matrix.size();
+5        int n = matrix[0].size();
+6
+7        for(int i = 0; i < m; i++) {
+8            for(int j = i; j < n; j++) {
+9                swap(matrix[i][j], matrix[j][i]);
+10            }
+11        }
+12
+13        for(int i = 0; i < m; i++) {
+14            reverse(matrix[i].begin(), matrix[i].end());
+15        }
+16    }
+17};
