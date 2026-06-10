8dfb6e7217bebc961088352993fc8439258ca790
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-10 09:05:08 +0530

    File updated at 6/10/2026, 9:05:08 AM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.cpp
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.cpp
@@ -1,38 +1,33 @@
-// Last updated: 6/10/2026, 9:02:22 AM
+// Last updated: 6/10/2026, 9:05:08 AM
 1class Solution {
 2public:
 3    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-4
-5        int size = flowerbed.size();
-6        int k = 0;  
-7
-8        if (size == 1) {
-9            if (flowerbed[0] == 0) k = 1;
-10            return k >= n;
-11        }
-12
-13        if (flowerbed[0] == 0 && flowerbed[1] == 0) {
-14            k++;
-15            flowerbed[0] = 1; 
-16        }
-17
-18        for (int i = 1; i < size - 1; i++) {
-19            if (flowerbed[i] == 0 &&
-20                flowerbed[i - 1] == 0 &&
-21                flowerbed[i + 1] == 0) 
-22            {
-23                k++;
-24                flowerbed[i] = 1; 
-25            }
-26        }
-27
-28        
-29        if (flowerbed[size - 1] == 0 && flowerbed[size - 2] == 0) {
-30            k++;
-31            
-32        }
-33
-34        return k >= n;
-35    }
-36};
-37
+4        int m = flowerbed.size();
+5        if(m == 1) {
+6            if(n == 1) {
+7                if(flowerbed[0] == 0) return true;
+8            } else if(n == 0) return true;
+9            else {
+10                return false;
+11            }
+12        }
+13
+14        if(flowerbed[0] == 0 && flowerbed[1] == 0) {
+15            n--;
+16            flowerbed[0] = 1;
+17        }
+18        if(flowerbed[m - 1] == 0 && flowerbed[m - 2] == 0) {
+19            n--;
+20            flowerbed[m - 1] = 1;
+21        }
+22
+23        for(int i = 1; i < m - 1; i++) {
+24            if(flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+25                flowerbed[i] = 1;
+26                n--;
+27            } 
+28        }
+29        cout << n << "\n";
+30        return n <= 0;
+31    }
+32};
