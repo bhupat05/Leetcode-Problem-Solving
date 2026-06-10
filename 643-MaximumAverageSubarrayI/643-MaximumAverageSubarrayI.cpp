67b340134c822ba043d47f762a737aeb67ce7a44
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-10 17:02:45 +0530

    File updated at 6/10/2026, 5:02:45 PM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.cpp
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.cpp
@@ -1,31 +1,21 @@
-// Last updated: 6/10/2026, 4:32:43 PM
+// Last updated: 6/10/2026, 5:02:45 PM
 1class Solution {
 2public:
-3    bool help(char c) {
-4        if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
-5           c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-6            return true;
-7           }
-8        return false;
-9    }
-10    int maxVowels(string s, int k) {
-11        int n = s.size();
-12        int ans = 0;
-13        int curr = 0;
-14        int l = 0;
-15
-16        for(int i = 0; i < k; i++) {
-17            if(help(s[i])) {
-18                curr++;
-19            }
-20        }
-21        ans = curr;
-22
-23        for(int i = k; i < n; i++) {
-24            curr += (help(s[i]) ? 1 : 0) - (help(s[l]) ? 1 : 0);
-25            ans = max(ans, curr);
-26            l++;
-27        }
-28        return ans;
-29    }
-30};
+3    int longestOnes(vector<int>& nums, int k) {
+4        int n = nums.size();
+5        int l = 0;
+6        int ans = 0;
+7        int zero = 0;
+8
+9        for(int i = 0; i < n; i++) {
+10            if(nums[i] == 0) zero++;
+11
+12            while(zero > k) {
+13                if(nums[l] == 0) zero--;
+14                l++;
+15            }
+16            ans = max(ans, i - l + 1);
+17        }   
+18        return ans;
+19    }
+20};
