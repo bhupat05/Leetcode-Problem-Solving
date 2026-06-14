183514b22134ccf783156faab824996085577cb9
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-14 17:08:47 +0530

    File updated at 6/14/2026, 5:08:46 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.cpp
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -1,20 +1,19 @@
-// Last updated: 6/14/2026, 12:15:18 PM
+// Last updated: 6/14/2026, 5:08:46 PM
 1class Solution {
 2public:
-3    int tribonacci(int n) {
-4        int a = 0;
-5        int b = 1;
-6        int c = 1;
-7        if(n == 0) return 0;
-8        if(n <= 2) return 1;
-9
-10        for(int i = 3; i <= n; i++) {
-11            int x = a + b + c;
-12            a = b;
-13            b = c;
-14            c = x;
+3    int rob(vector<int>& nums) {
+4        int n = nums.size();
+5        if(n == 1) return nums[0];
+6        if(n == 2) return max(nums[0], nums[1]);
+7
+8        int a = nums[0];
+9        int b = max(nums[0], nums[1]);
+10
+11        for(int i = 2; i < n; i++) {
+12            int c = max(a + nums[i], b);
+13            a = b;
+14            b = c;
 15        }
-16        return c;
-17
-18    }
-19};
+16        return b;
+17    }
+18};
