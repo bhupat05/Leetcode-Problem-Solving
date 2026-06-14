0897662b8a4cc3aaf4a67fc871464843ce2c60f1
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-14 11:48:51 +0530

    File updated at 6/14/2026, 11:48:51 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.cpp
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -1,27 +1,33 @@
-// Last updated: 3/18/2026, 2:29:09 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode *next = NULL;
-        ListNode *curr = head;
-        ListNode *pre = NULL;
-
-        while(curr != NULL){
-            next = curr -> next;
-            curr -> next = pre;
-            pre = curr;
-            curr = next;
-        }
-        return pre;
-    }
-};
+// Last updated: 6/14/2026, 11:48:51 AM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14
+15    void help(TreeNode* root, vector<int>& v) {
+16        if(!root) return;
+17        if(!root -> left && ! root -> right) {
+18            v.push_back(root -> val);
+19        }
+20        help(root -> left, v);
+21        help(root -> right, v);
+22    }
+23
+24    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+25        vector<int> v1;
+26        vector<int> v2;
+27        help(root1, v1);
+28        help(root2, v2);
+29
+30        return v1 == v2;
+31    }
+32};
