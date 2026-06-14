2085e1c606cd574c141d747a5683ebc9b4b3ef9c
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-14 12:15:18 +0530

    File updated at 6/14/2026, 12:15:18 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.cpp
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -1,27 +1,20 @@
-// Last updated: 6/14/2026, 12:02:28 PM
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    int help(TreeNode* root, int mx) {
-15        if(!root) return 0;
-16        if(root -> val < mx) return help(root->left, mx) + help(root->right, mx);
-17        
-18        mx = root -> val;
-19        return 1 + help(root -> left, mx) + help(root -> right, mx);
-20        
-21    }
-22    int goodNodes(TreeNode* root) {
-23        
-24        return help(root, root -> val);
-25    }
-26};
+// Last updated: 6/14/2026, 12:15:18 PM
+1class Solution {
+2public:
+3    int tribonacci(int n) {
+4        int a = 0;
+5        int b = 1;
+6        int c = 1;
+7        if(n == 0) return 0;
+8        if(n <= 2) return 1;
+9
+10        for(int i = 3; i <= n; i++) {
+11            int x = a + b + c;
+12            a = b;
+13            b = c;
+14            c = x;
+15        }
+16        return c;
+17
+18    }
+19};
