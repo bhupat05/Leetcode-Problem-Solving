409e7ca37fdd464effe29fc036e6ea9c1095fc9f
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-14 17:14:30 +0530

    File updated at 6/14/2026, 5:14:30 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.cpp
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -1,19 +1,14 @@
-// Last updated: 6/14/2026, 5:08:46 PM
+// Last updated: 6/14/2026, 5:14:30 PM
 1class Solution {
 2public:
-3    int rob(vector<int>& nums) {
-4        int n = nums.size();
-5        if(n == 1) return nums[0];
-6        if(n == 2) return max(nums[0], nums[1]);
-7
-8        int a = nums[0];
-9        int b = max(nums[0], nums[1]);
-10
-11        for(int i = 2; i < n; i++) {
-12            int c = max(a + nums[i], b);
-13            a = b;
-14            b = c;
-15        }
-16        return b;
-17    }
-18};
+3    int uniquePaths(int m, int n) {
+4        vector<vector<int>>dp(m, vector<int>(n, 1));
+5
+6        for(int i = 1; i < m; i++) {
+7            for(int j = 1; j < n; j++) {
+8                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+9            }
+10        }
+11        return dp[m - 1][n - 1];
+12    }
+13};
