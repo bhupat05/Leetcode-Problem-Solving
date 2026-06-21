361e5d7d6310731cb9394a25786d109ea6b3191d
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-21 15:48:00 +0530

    File updated at 6/21/2026, 3:47:59 PM

--- 338-CountingBits/338-CountingBits.cpp
+++ 338-CountingBits/338-CountingBits.cpp
@@ -1,14 +1,21 @@
-// Last updated: 3/18/2026, 2:28:03 PM
-class Solution {
-public:
-    vector<int> countBits(int n) {
-        vector<int> t(n + 1);
-
-        t[0] = 0;
-
-        for(int i = 0; i <=n; i++){
-            t[i] = t[i/2] + i % 2;
-        }
-        return t;
-    }
-};
+// Last updated: 6/21/2026, 3:47:59 PM
+1class Solution {
+2public:
+3    vector<int> countBits(int n) {
+4        vector<int> r;
+5
+6        for (int i = 0; i <= n; i++) {
+7            int num = i;
+8            int x = 0;
+9
+10            while (num > 0) {
+11                x += (num & 1); 
+12                num /= 2;       
+13            }
+14
+15            r.push_back(x);
+16        }
+17
+18        return r;
+19    }
+20};
