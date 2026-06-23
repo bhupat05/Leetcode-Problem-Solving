6f9209a2eed7c0346922356aec203bec185f3f6f
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-23 21:10:28 +0530

    File updated at 6/23/2026, 9:10:28 PM

--- 9-PalindromeNumber/9-PalindromeNumber.cpp
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -1,21 +1,18 @@
-// Last updated: 3/18/2026, 3:03:17 PM
-class Solution {
-public:
-    bool isPalindrome(int t) {
-        int x = t;
-        if (x < 0) return false;
-        if(x == 0) return true;
-        
-        long long y = 0;
-        int r = 0;
-        while(x != 0){
-            
-            r = x % 10;
-            y = 10*y + r;
-            x /= 10;
-        }
-       
-        if(t == y) return true;
-        else return false;
-    }
-};
+// Last updated: 6/23/2026, 9:10:28 PM
+1class Solution {
+2public:
+3    bool isPalindrome(int x) {
+4        if(x < 0) return false;
+5
+6        string s = to_string(x);
+7        int l = 0;
+8        int r = s.size() - 1;
+9
+10        while(l <= r) {
+11            if(s[l] != s[r]) return false;
+12            l++;
+13            r--;
+14        }
+15        return true;
+16    }
+17};
