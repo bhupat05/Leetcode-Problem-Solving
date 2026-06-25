93227560f92b0ddcc93d21bb33dae2af53ea3c23
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-25 16:05:24 +0530

    File updated at 6/25/2026, 4:05:23 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
@@ -1,19 +1,21 @@
-// Last updated: 6/25/2026, 2:51:01 PM
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    int maxDepth(TreeNode* root) {
-15       if(root == nullptr) return 0;
-16       return 1 + max(maxDepth(root->left),maxDepth(root->right)); 
-17    }
-18};
+// Last updated: 6/25/2026, 4:05:23 PM
+1class Solution {
+2public:
+3    vector<int> dailyTemperatures(vector<int>& temperatures) {
+4        int n = temperatures.size();
+5        vector<int> r(n, 0);
+6        stack<int> st;
+7
+8        for(int i = n - 1; i >= 0; i--) {
+9            while(!st.empty() && temperatures[st.top()] <= temperatures[i]) {
+10                st.pop();
+11            }
+12
+13            if(!st.empty()) {
+14                r[i] = st.top() - i;
+15            }
+16            st.push(i);
+17        }
+18        return r;
+19    }
+20};
