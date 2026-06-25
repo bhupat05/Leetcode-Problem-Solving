49bb7e41255634e401ea727e9b9e2d28ce27b4fb
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-25 14:51:01 +0530

    File updated at 6/25/2026, 2:51:01 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 6/24/2026, 9:43:22 AM
+// Last updated: 6/25/2026, 2:51:01 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -13,25 +13,7 @@
 12class Solution {
 13public:
 14    int maxDepth(TreeNode* root) {
-15        if(!root) return 0;
-16
-17        queue<TreeNode*> q;
-18        q.push(root);
-19
-20        int depth = 0;
-21
-22        while(!q.empty()) {
-23            int sz = q.size();
-24            depth++;
-25
-26            for(int i = 0; i < sz; i++) {
-27                TreeNode* node = q.front();
-28                q.pop();
-29
-30                if(node -> left) q.push(node -> left);
-31                if(node -> right) q.push(node -> right);
-32            }
-33        }
-34        return depth;
-35    }
-36};
+15       if(root == nullptr) return 0;
+16       return 1 + max(maxDepth(root->left),maxDepth(root->right)); 
+17    }
+18};
