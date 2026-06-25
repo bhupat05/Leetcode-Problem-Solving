4613b330e93168006b34c53747265c4b87e9a83e
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-25 17:18:30 +0530

    File updated at 6/25/2026, 5:18:30 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
@@ -1,21 +1,29 @@
-// Last updated: 6/25/2026, 4:05:23 PM
+// Last updated: 6/25/2026, 5:18:30 PM
 1class Solution {
 2public:
-3    vector<int> dailyTemperatures(vector<int>& temperatures) {
-4        int n = temperatures.size();
-5        vector<int> r(n, 0);
-6        stack<int> st;
-7
-8        for(int i = n - 1; i >= 0; i--) {
-9            while(!st.empty() && temperatures[st.top()] <= temperatures[i]) {
-10                st.pop();
-11            }
-12
-13            if(!st.empty()) {
-14                r[i] = st.top() - i;
-15            }
-16            st.push(i);
-17        }
-18        return r;
-19    }
-20};
+3    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+4        int n = intervals.size();
+5        sort(intervals.begin(), intervals.end());
+6        for(auto it : intervals) {
+7            cout << it[0] <<" " << it[1] << "\n";
+8        }
+9        int cnt = 0;
+10        int l = 0;
+11        int r = 0;
+12        
+13        for(int i = 1; i < n; i++) {
+14            if(intervals[l][1] > intervals[i][0]) cnt++;
+15            else l = i;
+16        }
+17        r = cnt;
+18        cnt = 0;
+19        l = n - 1;
+20
+21         for(int i = n - 2; i >= 0; i--) {
+22            if(intervals[l][0] < intervals[i][1]) cnt++;
+23            else l = i;
+24        }
+25
+26        return min(r, cnt);
+27    }
+28};
