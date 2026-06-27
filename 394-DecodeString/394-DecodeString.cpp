ba607844d99409a8a6401efda915f1cf63547c9b
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-27 22:12:09 +0530

    File updated at 6/27/2026, 10:12:09 PM

--- 394-DecodeString/394-DecodeString.cpp
+++ 394-DecodeString/394-DecodeString.cpp
@@ -1,51 +1,28 @@
-// Last updated: 6/27/2026, 7:18:56 PM
-1class Solution {
-2public:
-3    string decodeString(string s) {
-4        int n = s.size();
-5        stack<char> st;
-6        string r = "";
-7        
-8        for(int i = 0; i < n; i++) {
-9            if(s[i] == ']') {
-10                string temp = "";
-11                int cnt = 0;
-12
-13                while(!st.empty() && st.top() != '[') {
-14                    temp += st.top();
-15                    st.pop();
-16                }
-17                reverse(temp.begin(), temp.end());
-18                st.pop();
-19                string te = "";
-20                while(!st.empty() && isdigit(st.top())) {
-21                    te += st.top();
-22                    st.pop();
-23                }
-24                reverse(te.begin(), te.end());
-25                cnt = stoi(te);
-26            
-27                string t = "";
-28
-29                for(int i = 0; i < cnt; i++) {
-30                    t += temp;
-31                }
-32                
-33                for(int j = 0; j < t.size(); j++) {
-34                    st.push(t[j]);
-35                }
-36
-37
-38            } else {
-39                st.push(s[i]);
-40            }
-41        }
-42
-43        while(!st.empty()) {
-44            r += st.top();
-45            st.pop();
-46        }
-47        reverse(r.begin(), r.end());
-48        return r;
-49    }
-50};
+// Last updated: 6/27/2026, 10:12:09 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    void help(TreeNode* root, int level, vector<int>& r) {
+15        if(!root) return;
+16        if(r.size() < level) {
+17            r.push_back(root -> val);
+18        }
+19        help(root -> right, level + 1, r);
+20        help(root -> left, level + 1, r);
+21    }
+22    vector<int> rightSideView(TreeNode* root) {
+23        vector<int> r;
+24        help(root, 1, r);
+25        return r;
+26    }
+27};
