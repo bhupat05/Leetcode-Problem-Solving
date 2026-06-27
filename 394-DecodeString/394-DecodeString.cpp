7f710a9cb0e80e5594575d00d7413e105139dda7
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-27 19:18:56 +0530

    File updated at 6/27/2026, 7:18:56 PM

--- 394-DecodeString/394-DecodeString.cpp
+++ 394-DecodeString/394-DecodeString.cpp
@@ -1,42 +1,51 @@
-// Last updated: 3/18/2026, 2:27:44 PM
-class Solution {
-public:
-    string decodeString(string s) {
-        stack<int> numStack;       
-        stack<string> strStack;    
-        string current = "";       
-        int num = 0;               
-
-        for(char c : s) {
-
-            if(isdigit(c)) {
-                num = num * 10 + (c - '0');
-            }
-
-            else if(c == '[') {
-                numStack.push(num);
-                strStack.push(current);
-
-                num = 0;
-                current = "";
-            }
-
-            else if(c == ']') {
-                string temp = current;
-
-                current = strStack.top(); strStack.pop();
-                int repeat = numStack.top(); numStack.pop();
-
-                while(repeat--) {
-                    current += temp;
-                }
-            }
-
-            else {
-                current += c;
-            }
-        }
-
-        return current;
-    }
-};
+// Last updated: 6/27/2026, 7:18:56 PM
+1class Solution {
+2public:
+3    string decodeString(string s) {
+4        int n = s.size();
+5        stack<char> st;
+6        string r = "";
+7        
+8        for(int i = 0; i < n; i++) {
+9            if(s[i] == ']') {
+10                string temp = "";
+11                int cnt = 0;
+12
+13                while(!st.empty() && st.top() != '[') {
+14                    temp += st.top();
+15                    st.pop();
+16                }
+17                reverse(temp.begin(), temp.end());
+18                st.pop();
+19                string te = "";
+20                while(!st.empty() && isdigit(st.top())) {
+21                    te += st.top();
+22                    st.pop();
+23                }
+24                reverse(te.begin(), te.end());
+25                cnt = stoi(te);
+26            
+27                string t = "";
+28
+29                for(int i = 0; i < cnt; i++) {
+30                    t += temp;
+31                }
+32                
+33                for(int j = 0; j < t.size(); j++) {
+34                    st.push(t[j]);
+35                }
+36
+37
+38            } else {
+39                st.push(s[i]);
+40            }
+41        }
+42
+43        while(!st.empty()) {
+44            r += st.top();
+45            st.pop();
+46        }
+47        reverse(r.begin(), r.end());
+48        return r;
+49    }
+50};
