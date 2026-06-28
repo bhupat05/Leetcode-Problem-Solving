c1f71545e176457f1a3f0bcb25e80311264abee0
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-06-28 07:39:47 +0530

    File updated at 6/28/2026, 7:39:47 AM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.java
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.java
@@ -1,21 +1,31 @@
-// Last updated: 6/28/2026, 6:57:03 AM
-1class RecentCounter {
-2    Queue<Integer> q = new LinkedList<>();
-3    public RecentCounter() {
-4        
-5    }
-6    
-7    public int ping(int t) {
-8        q.add(t);
-9        while(!q.isEmpty() && q.peek() < t - 3000) {
-10            q.poll();
-11        } 
-12        return q.size();
-13    }
-14}
+// Last updated: 6/28/2026, 7:39:47 AM
+1class Solution {
+2    public String predictPartyVictory(String senate) {
+3
+4        int n = senate.length();
+5
+6        Queue<Integer> radiant = new LinkedList<>();
+7        Queue<Integer> dire = new LinkedList<>();
+8
+9        for (int i = 0; i < n; i++) {
+10            if (senate.charAt(i) == 'R')
+11                radiant.add(i);
+12            else
+13                dire.add(i);
+14        }
 15
-16/**
-17 * Your RecentCounter object will be instantiated and called as such:
-18 * RecentCounter obj = new RecentCounter();
-19 * int param_1 = obj.ping(t);
-20 */
+16        while (!radiant.isEmpty() && !dire.isEmpty()) {
+17
+18            int r = radiant.poll();
+19            int d = dire.poll();
+20
+21            if (r < d) {
+22                radiant.add(r + n);
+23            } else {
+24                dire.add(d + n);
+25            }
+26        }
+27
+28        return radiant.isEmpty() ? "Dire" : "Radiant";
+29    }
+30}
