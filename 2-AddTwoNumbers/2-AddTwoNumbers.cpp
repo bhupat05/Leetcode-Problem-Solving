2624aebdefa4c2c8af9c9aaca36dafc24bcee2bc
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-03 08:51:08 +0530

    File updated at 7/3/2026, 8:51:09 AM

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,32 +1,38 @@
-// Last updated: 3/18/2026, 3:03:27 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode *add(ListNode* l1, ListNode* l2, int carry) {
-        if(l1 == nullptr && l2 == nullptr && carry == 0) return nullptr;
-
-        int sum = carry;
-
-        if(l1) sum += l1 -> val;
-        if(l2) sum += l2 -> val;
-
-        ListNode* node = new ListNode(sum % 10);
-
-        node -> next = add(l1 ? l1 -> next : nullptr, l2 ? l2 -> next : nullptr, sum / 10);
-
-        return node;
-
-    }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return add(l1, l2, 0);
-    }
-};
+// Last updated: 7/3/2026, 8:51:09 AM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+14        ListNode* head = nullptr;
+15        ListNode* temp = nullptr;
+16        int carry = 0;
+17        while(l1 || l2 || carry) {
+18
+19            int sum = (l1 ? l1 -> val : 0) + (l2 ? l2 -> val : 0) + carry;
+20            int x = sum <= 9 ? sum : sum % 10;
+21            ListNode* node = new ListNode(x);
+22            carry = sum > 9 ? sum / 10 : 0;
+23
+24            l1 = l1 ? l1 -> next : nullptr;
+25            l2 = l2 ? l2 -> next : nullptr;
+26            if(temp == nullptr) {
+27                head = node;
+28                temp = node;
+29            } else {
+30                head->next = node;
+31                head = node;
+32            }
+33            
+34        }
+35        return temp;
+36    }
+37};
