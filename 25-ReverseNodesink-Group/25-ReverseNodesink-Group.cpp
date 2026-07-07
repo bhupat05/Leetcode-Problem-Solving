43e5a264121ba10a6ddb3cb487ac8d9e0a321bd4
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 01:00:05 +0530

    File updated at 7/8/2026, 1:00:05 AM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.cpp
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.cpp
@@ -1,39 +1,48 @@
-// Last updated: 7/7/2026, 11:35:47 PM
-1class Solution {
-2public:
-3    bool check(string &p, int j) {
-4        
-5        for(int i = 0; i <= j; i++) {
-6            if(p[i] != '*') return false;
-7        }
-8        return true;
-9        
-10    }
-11    bool solve(string &s, string &p, int i, int j, vector<vector<int>>& dp) {
-12        if(i == -1 && j == -1) return true;
-13        if(j == 0 && p[j] == '*') return true;
-14        if(i == -1) {
-15            return check(p, j);
+// Last updated: 7/8/2026, 1:00:05 AM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* reversell(ListNode* head){
+14        if(head == NULL || head->next == NULL){
+15            return head;
 16        }
-17        if(j == -1) return false;
-18
-19        if(dp[i][j] != -1) return dp[i][j];
-20
-21        if((s[i] == p[j]) || (p[j] == '?')) {
-22            return dp[i][j] = solve(s, p, i - 1, j - 1, dp);
-23        } else if(p[j] == '*') {
-24            
-25            return dp[i][j] = solve(s, p, i - 1, j, dp) || solve(s, p, i, j - 1, dp);
-26        } else {
-27            return false;
-28        }
-29        return false;
-30    }
-31    bool isMatch(string s, string p) {
-32        int m = s.size();
-33        int n = p.size();
-34        vector<vector<int>> dp(m, vector<int>(n, -1));
-35        
-36        return solve(s, p, m - 1, n - 1, dp);
-37    }
-38};
+17
+18        ListNode* last = reversell(head->next);
+19        head->next->next = head;
+20        head->next = NULL;
+21        return last;
+22    }
+23    void reorderList(ListNode* head) {
+24        ListNode* slow = head;
+25        ListNode* fast = head;
+26
+27        while(fast && fast ->next){
+28            slow = slow -> next;
+29            fast = fast -> next -> next;
+30        }
+31
+32        ListNode* rev = reversell(slow);
+33        ListNode* curr = head;
+34
+35        while(rev -> next != NULL){
+36            ListNode* tempcurr = curr -> next;
+37            curr ->next = rev;
+38
+39            ListNode* temprev = rev -> next;
+40            rev -> next = tempcurr;
+41
+42            curr = tempcurr;
+43            rev = temprev;
+44        }
+45        
+46    }
+47};
