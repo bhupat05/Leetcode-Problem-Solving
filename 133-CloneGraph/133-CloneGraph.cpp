259a40cb4ed5421aeafce0e6219eceb09c2b0c43
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 18:43:10 +0530

    File updated at 7/8/2026, 6:43:10 PM

--- 133-CloneGraph/133-CloneGraph.cpp
+++ 133-CloneGraph/133-CloneGraph.cpp
@@ -1,34 +1,57 @@
-// Last updated: 3/18/2026, 2:30:10 PM
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() : val(0), neighbors() {}
-    Node(int _val) : val(_val), neighbors() {}
-    Node(int _val, vector<Node*> _neighbors) : val(_val), neighbors(_neighbors) {}
-};
-*/
-
-class Solution {
-public:
-    Node* cloneGraph(Node* node) {
-        if (!node) return nullptr;
-        unordered_map<Node*, Node*> mp;
-        queue<Node*> q;
-        mp[node] = new Node(node->val);
-        q.push(node);
-        while (!q.empty()) {
-            Node* cur = q.front(); q.pop();
-            for (Node* nei : cur->neighbors) {
-                if (!mp.count(nei)) {
-                    mp[nei] = new Node(nei->val);
-                    q.push(nei);
-                }
-                mp[cur]->neighbors.push_back(mp[nei]);
-            }
-        }
-        return mp[node];
-    }
-};
+// Last updated: 7/8/2026, 6:43:10 PM
+1/*
+2// Definition for a Node.
+3class Node {
+4public:
+5    int val;
+6    vector<Node*> neighbors;
+7    Node() {
+8        val = 0;
+9        neighbors = vector<Node*>();
+10    }
+11    Node(int _val) {
+12        val = _val;
+13        neighbors = vector<Node*>();
+14    }
+15    Node(int _val, vector<Node*> _neighbors) {
+16        val = _val;
+17        neighbors = _neighbors;
+18    }
+19};
+20*/
+21
+22class Solution {
+23public:
+24    unordered_map<Node*, Node*> mp;
+25    Node* cloneGraph(Node* node) {
+26        if(!node) return nullptr;
+27
+28        if(mp.count(node)) return mp[node];
+29
+30        Node* clone = new Node(node -> val);
+31        mp[node] = clone;
+32
+33        for(auto nbr : node -> neighbors) {
+34            (clone -> neighbors).push_back(cloneGraph(nbr));
+35        }
+36        return clone;
+37    }
+38};
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
