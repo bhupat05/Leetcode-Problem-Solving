8d4b7b0f66705fddfd40b94be118c687577f3118
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 14:23:21 +0530

    File updated at 7/8/2026, 2:23:21 PM

--- 200-NumberofIslands/200-NumberofIslands.cpp
+++ 200-NumberofIslands/200-NumberofIslands.cpp
@@ -1,29 +1,34 @@
-// Last updated: 7/8/2026, 2:19:31 PM
+// Last updated: 7/8/2026, 2:23:21 PM
 1class Solution {
 2public:
-3    void dfs(vector<vector<char>>& grid, int i, int j) {
-4        int m = grid.size();
-5        int n = grid[0].size();
-6        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
-7
-8        grid[i][j] = '0';
-9        dfs(grid, i, j + 1);
-10        dfs(grid, i, j - 1);
-11        dfs(grid, i + 1, j);
-12        dfs(grid, i - 1, j);
-13    }
-14    int numIslands(vector<vector<char>>& grid) {
-15        int m = grid.size();
-16        int n = grid[0].size();
-17        int cnt = 0;
-18        for(int i = 0; i < m; i++) {
-19            for(int j = 0; j < n; j++) {
-20                if(grid[i][j] == '1') {
-21                    cnt++;
-22                    dfs(grid, i, j);
-23                }
-24            }
-25        }
-26        return cnt;
-27    }
-28};
+3    int dx[4] = {0, 0, 1, -1};
+4    int dy[4] = {1, -1, 0, 0};
+5    void dfs(vector<vector<char>>& grid, int i, int j) {
+6        int m = grid.size();
+7        int n = grid[0].size();
+8        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
+9
+10        grid[i][j] = '0';
+11
+12        for(int k = 0; k < 4; k++) {
+13            int ni = i + dx[k];
+14            int nj = j + dy[k];
+15            dfs(grid, ni, nj);
+16        }
+17        
+18    }
+19    int numIslands(vector<vector<char>>& grid) {
+20        int m = grid.size();
+21        int n = grid[0].size();
+22        int cnt = 0;
+23        for(int i = 0; i < m; i++) {
+24            for(int j = 0; j < n; j++) {
+25                if(grid[i][j] == '1') {
+26                    cnt++;
+27                    dfs(grid, i, j);
+28                }
+29            }
+30        }
+31        return cnt;
+32    }
+33};
