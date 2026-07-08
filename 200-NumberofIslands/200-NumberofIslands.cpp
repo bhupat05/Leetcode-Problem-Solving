1fc0f016024a397ab2cfa09bc3901fcb6d0b07d2
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 14:19:30 +0530

    File updated at 7/8/2026, 2:19:31 PM

--- 200-NumberofIslands/200-NumberofIslands.cpp
+++ 200-NumberofIslands/200-NumberofIslands.cpp
@@ -1,44 +1,29 @@
-// Last updated: 4/4/2026, 1:58:23 AM
+// Last updated: 7/8/2026, 2:19:31 PM
 1class Solution {
 2public:
-3    int dx[4] = {-1, 1, 0, 0};
-4    int dy[4] = {0, 0, -1, 1};
-5
-6    void bfs(vector<vector<char>>& grid, int i, int j) {
-7        int m = grid.size(), n = grid[0].size();
-8
-9        queue<pair<int,int>> q;
-10        q.push({i, j});
-11        grid[i][j] = '0';
-12
-13        while(!q.empty()) {
-14            auto [x, y] = q.front();
-15            q.pop();
-16
-17            for(int d = 0; d < 4; d++) {
-18                int nx = x + dx[d];
-19                int ny = y + dy[d];
-20
-21                if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]=='1') {
-22                    grid[nx][ny] = '0';
-23                    q.push({nx, ny});
-24                }
-25            }
-26        }
+3    void dfs(vector<vector<char>>& grid, int i, int j) {
+4        int m = grid.size();
+5        int n = grid[0].size();
+6        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
+7
+8        grid[i][j] = '0';
+9        dfs(grid, i, j + 1);
+10        dfs(grid, i, j - 1);
+11        dfs(grid, i + 1, j);
+12        dfs(grid, i - 1, j);
+13    }
+14    int numIslands(vector<vector<char>>& grid) {
+15        int m = grid.size();
+16        int n = grid[0].size();
+17        int cnt = 0;
+18        for(int i = 0; i < m; i++) {
+19            for(int j = 0; j < n; j++) {
+20                if(grid[i][j] == '1') {
+21                    cnt++;
+22                    dfs(grid, i, j);
+23                }
+24            }
+25        }
+26        return cnt;
 27    }
-28
-29    int numIslands(vector<vector<char>>& grid) {
-30        int m = grid.size(), n = grid[0].size();
-31        int cnt = 0;
-32
-33        for(int i = 0; i < m; i++) {
-34            for(int j = 0; j < n; j++) {
-35                if(grid[i][j] == '1') {
-36                    bfs(grid, i, j);
-37                    cnt++;
-38                }
-39            }
-40        }
-41        return cnt;
-42    }
-43};
+28};
