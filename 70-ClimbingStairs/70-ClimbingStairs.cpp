737691e4a633f48de7d62cba1aaf9b5c958439f1
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 08:44:13 +0530

    File updated at 7/8/2026, 8:44:14 AM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,17 +1,14 @@
-// Last updated: 3/18/2026, 3:02:02 PM
-class Solution {
-public:
-    int climbStairs(int n) {
-        if(n == 0 || n == 1) return 1;
-
-        int a = 1;
-        int b = 1;
-
-        for(int i=2; i<=n; i++){
-            int c = a + b;
-            b = a;
-            a = c;
-        }
-        return a;
-    }
-};
+// Last updated: 7/8/2026, 8:44:14 AM
+1class Solution {
+2public:
+3    int solve(int n, vector<int>& dp) {
+4        if(n <= 2) return n;
+5        if(dp[n] != -1) return dp[n];
+6
+7        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+8    }
+9    int climbStairs(int n) {
+10        vector<int> dp(n + 1, -1);
+11        return solve(n, dp);
+12    }
+13};
