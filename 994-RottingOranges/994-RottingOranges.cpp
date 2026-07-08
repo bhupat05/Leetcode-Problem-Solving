f7ee2f5d03f4aa837cb9dd0981be274ac4d82322
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 22:19:45 +0530

    File updated at 7/8/2026, 10:19:45 PM

--- 994-RottingOranges/994-RottingOranges.cpp
+++ 994-RottingOranges/994-RottingOranges.cpp
@@ -1,56 +1,59 @@
-// Last updated: 4/4/2026, 11:24:30 AM
+// Last updated: 7/8/2026, 10:19:45 PM
 1class Solution {
 2public:
-3    int dx[4] = {-1, 1, 0, 0};
-4    int dy[4] = {0, 0, -1, 1};
-5    int ans = 0;
-6    int m;
-7    int n;
-8
-9    void bfs(vector<vector<int>>& grid) {
-10        queue<pair<int, int>> q;
-11
+3    int dx[4] = {1, -1, 0, 0};
+4    int dy[4] = {0, 0, 1, -1};
+5    int orangesRotting(vector<vector<int>>& grid) {
+6        int m = grid.size();
+7        int n = grid[0].size();
+8        int ans = 0;
+9        queue<pair<int, int>> q;
+10        int cnt = 0;
+11        
 12        for(int i = 0; i < m; i++) {
 13            for(int j = 0; j < n; j++) {
 14                if(grid[i][j] == 2) {
 15                    q.push({i, j});
-16                }
+16                } else if(grid[i][j] == 1) cnt++;
 17            }
 18        }
-19
-20        while(!q.empty()) {
-21            int sz = q.size();
-22
-23            while(sz--) {
-24                auto[x, y] = q.front();
-25                q.pop();
-26                for(int i = 0; i < 4; i++) {
-27                    int ni = x + dx[i];
-28                    int nj = y + dy[i];
+19        
+20        if(cnt <= 0) return ans;
+21        
+22        while(!q.empty()) {
+23            int sz = q.size();
+24            if(cnt <= 0) return ans;
+25            
+26            while(sz--) {
+27                auto[i, j] = q.front();
+28                q.pop();
 29
-30                    if(ni >= 0 && nj >= 0 && ni < m && nj < n  && grid[ni][nj] == 1) {
-31                        q.push({ni, nj});
-32                        grid[ni][nj] = 2;
-33                    } 
-34                }
-35            }
-36            ans++;
-37        }
-38    }
-39
-40    int orangesRotting(vector<vector<int>>& grid) {
-41        m = grid.size();
-42        n = grid[0].size();
+30                for(int k = 0; k < 4; k++) {
+31                    int ni = i + dx[k];
+32                    int nj = j + dy[k];
+33
+34                    if(ni < 0 || ni >= m || nj < 0 || nj >= n) continue;
+35
+36                    if(grid[ni][nj] != 1) continue;
+37
+38                    grid[ni][nj] = 2;
+39                    cnt--;
+40
+41                    q.push({ni, nj});
+42                }
 43
-44        bfs(grid);
-45        cout << ans << " ";
-46
-47        for(int i = 0; i < m; i++) {
-48            for(int j = 0; j < n; j++) {
-49                if(grid[i][j] == 1) return -1;
-50            }
-51        }
-52       
-53        return (ans == 0 ? 0 : ans - 1);
-54    }
-55};
+44            }
+45           
+46            ans++;
+47            
+48        }
+49
+50        // for(int i = 0; i < m; i++) {
+51        //     for(int j = 0; j < n; j++) {
+52        //         if(grid[i][j] == 1) return -1;
+53        //     }
+54        // }
+55        cout << cnt;
+56        return -1;
+57    }
+58};
