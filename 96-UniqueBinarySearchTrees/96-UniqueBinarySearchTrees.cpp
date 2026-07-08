301082e14c772ad5737ed8adcaaafb3379361281
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 11:22:01 +0530

    File updated at 7/8/2026, 11:22:02 AM

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.cpp
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.cpp
@@ -1,19 +1,19 @@
-// Last updated: 3/18/2026, 3:01:29 PM
-class Solution {
-public:
-    long long catalan(int n) {
-        long long res = 1;
-
-        // Calculate C(2n, n) step by step
-        for (int i = 1; i <= n; i++) {
-            res = res * (n + i) / i;
-        }
-
-        // Catalan number = C(2n, n) / (n + 1)
-        return res / (n + 1);
-    }
-
-    int numTrees(int n) {
-        return (int)catalan(n);
-    }
-};
+// Last updated: 7/8/2026, 11:22:02 AM
+1class Solution {
+2public:
+3    int solve(int n, vector<int>& dp) {
+4        if(n == 0) return 1;   
+5        if(n == 1) return 1;
+6
+7        int ans = 0;
+8        for(int i = 0; i < n; i++) {
+9            ans += solve(i, dp) * solve(n - 1 - i, dp);  
+10        }
+11        return dp[n] = ans;
+12    }
+13
+14    int numTrees(int n) {
+15        vector<int> dp(n + 1, -1);
+16        return solve(n, dp);
+17    }
+18};
