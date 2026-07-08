fbc6bede529750d6fbc9bdf153b128d6d56c8835
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-08 10:47:03 +0530

    File updated at 7/8/2026, 10:47:04 AM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,16 +1,15 @@
-// Last updated: 3/18/2026, 3:02:12 PM
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        long long r = 1;
-
-        int k = min(m - 1, n - 1);
-        int total = m + n - 2;
-
-        for(int i = 1; i <= k; i++) {
-            r = r * (total - k + i) / i;
-        }
-
-        return (int)r;
-    }
-};
+// Last updated: 7/8/2026, 10:47:04 AM
+1class Solution {
+2public:
+3    int solve(int m, int n, vector<vector<int>>& dp) {
+4        if(m == 0 || n == 0) return 1;
+5        
+6        if(dp[m][n] != -1) return dp[m][n];
+7
+8        return dp[m][n] = solve(m - 1, n, dp) + solve(m, n - 1, dp);
+9    }
+10    int uniquePaths(int m, int n) {
+11        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+12        return solve(m - 1, n - 1, dp);   
+13    }
+14};
