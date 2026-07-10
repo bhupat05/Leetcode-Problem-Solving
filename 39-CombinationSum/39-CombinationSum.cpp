d02f7c6025f933db043c250474ba2cf4469a8e9d
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-10 07:10:28 +0530

    File updated at 7/10/2026, 7:10:29 AM

--- 39-CombinationSum/39-CombinationSum.cpp
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -1,29 +1,28 @@
-// Last updated: 3/18/2026, 3:02:39 PM
-class Solution {
-public:
-    void solve(vector<int>& candidates, int i, int target,
-               vector<int>& combination, vector<vector<int>>& ans) {
-        
-        if (target == 0) {
-            ans.push_back(combination);
-            return;
-        }
-
-        if (i == candidates.size() || target < 0)
-            return;
-
-        
-        combination.push_back(candidates[i]);
-        solve(candidates, i, target - candidates[i], combination, ans); 
-        combination.pop_back(); 
-
-        solve(candidates, i + 1, target, combination, ans);
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> combination;
-        solve(candidates, 0, target, combination, ans);
-        return ans;
-    }
-};
+// Last updated: 7/10/2026, 7:10:29 AM
+1class Solution {
+2public:
+3    vector<vector<int>> r;
+4    int n;
+5
+6    void solve(vector<int>& candidates, int idx, int target, vector<int>& temp) {
+7        if(idx == n) {
+8            if(target == 0) {
+9                r.push_back(temp);
+10            }
+11            return;
+12        }
+13        if(target < 0) return;
+14
+15        temp.push_back(candidates[idx]);
+16        solve(candidates, idx, target - candidates[idx], temp);
+17        temp.pop_back();
+18        solve(candidates, idx + 1, target, temp);
+19    }
+20
+21    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+22        n = candidates.size();
+23        vector<int> temp;
+24        solve(candidates, 0, target, temp);
+25        return r;
+26    }
+27};
