8dfe3270e610e67274317c321ba7c72f1bf82fd0
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-10 09:50:14 +0530

    File updated at 7/10/2026, 9:50:15 AM

--- 22-GenerateParentheses/22-GenerateParentheses.cpp
+++ 22-GenerateParentheses/22-GenerateParentheses.cpp
@@ -1,28 +1,32 @@
-// Last updated: 3/18/2026, 3:03:01 PM
-class Solution {
-public:
-    void solve(int open, int close, int n, string &temp, vector<string>& r) {
-        if(open == n && close == n) {
-            r.push_back(temp);
-            return;
-        }
-
-        if(open < n) {
-            temp.push_back('(');
-            solve(open + 1, close, n, temp, r);
-            temp.pop_back();
-        }
-
-        if(close < open) {
-            temp.push_back(')');
-            solve(open , close + 1, n, temp, r);
-            temp.pop_back();
-        }
-    }
-    vector<string> generateParenthesis(int n) {
-        vector<string> r;
-        string temp = "";
-        solve(0, 0 , n, temp,r);
-        return r;
-    }
-};
+// Last updated: 7/10/2026, 9:50:15 AM
+1class Solution {
+2public:
+3    vector<string> ans;
+4    void solve(int n, int l, int r, string& s) {
+5        if(l == n && r == n) {
+6            ans.push_back(s);
+7            return;
+8        }
+9
+10        if(l < n) {
+11            s.push_back('(');
+12            solve(n, l + 1, r, s);
+13            s.pop_back();
+14        }
+15        
+16
+17
+18        if(l > r) {
+19             s.push_back(')');;
+20            solve(n, l, r + 1, s);
+21            s.pop_back();
+22        }
+23    }
+24    vector<string> generateParenthesis(int n) {
+25        if(n == 0) return {""};
+26        string s;
+27       
+28        solve(n, 0, 0, s);
+29        return ans;
+30    }
+31};
