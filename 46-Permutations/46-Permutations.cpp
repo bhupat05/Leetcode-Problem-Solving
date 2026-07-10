549589f9a7a5391b7b27d39ca1d3852b69c46427
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-10 07:29:14 +0530

    File updated at 7/10/2026, 7:29:14 AM

--- 46-Permutations/46-Permutations.cpp
+++ 46-Permutations/46-Permutations.cpp
@@ -1,22 +1,24 @@
-// Last updated: 3/18/2026, 3:02:30 PM
-class Solution {
-public:
-    void solve(vector<int> &nums, int idx, vector<vector<int>> &ans){
-        if(idx == nums.size()){
-            ans.push_back({nums});
-            return;
-        }
-
-        for(int i = idx; i < nums.size(); i++){
-            swap(nums[idx], nums[i]);
-            solve(nums, idx + 1, ans);
-            swap(nums[idx], nums[i]);
-            
-        }
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        solve(nums, 0, ans);
-        return ans;
-    }
-};
+// Last updated: 7/10/2026, 7:29:14 AM
+1class Solution {
+2public:
+3    vector<vector<int>> r;
+4    int n;
+5    
+6    void solve(vector<int>& nums, int idx) {
+7        if(idx == n) {
+8            r.push_back(nums);
+9            return;
+10        }
+11        for(int i = idx; i < n; i++) {
+12            swap(nums[idx], nums[i]);
+13            solve(nums, idx + 1);
+14            swap(nums[idx], nums[i]);
+15        }
+16    }
+17
+18    vector<vector<int>> permute(vector<int>& nums) {
+19        n = nums.size();
+20        solve(nums, 0);
+21        return r;
+22    }
+23};
