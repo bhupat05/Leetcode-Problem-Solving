2dd8e2db0bbdfe80388dc61411dc71e420475e71
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-11 22:30:07 +0530

    File updated at 7/11/2026, 10:30:06 PM

--- 139-WordBreak/139-WordBreak.cpp
+++ 139-WordBreak/139-WordBreak.cpp
@@ -1,24 +1,23 @@
-// Last updated: 3/18/2026, 2:30:01 PM
-class Solution {
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
-        vector<bool> dp(n + 1, false);
-
-        dp[0] = true;
-
-        for(int i = 1; i <= n; i++) {
-            for(string& word : wordDict) {
-                int len = word.length();
-
-                if(i >= len && dp[i - len]) {
-                    if(s.substr(i - len, len) == word) {
-                        dp[i] = true;
-                        break;
-                    }
-                }
-            }
-        }
-        return dp[n];
-    }
-};
+// Last updated: 7/11/2026, 10:30:06 PM
+1class Solution {
+2public:
+3    int n;
+4    bool solve(string s, unordered_set<string>& st, int idx, vector<int>& dp) {
+5        if(idx == s.size()) return true;
+6        
+7        if(dp[idx] != -1) return dp[idx];
+8
+9        for(int i = idx; i < s.size(); i++) {
+10            string word = s.substr(idx, i - idx + 1);
+11            if(st.count(word) && solve(s, st, i + 1, dp)) return dp[idx] = true;
+12        }
+13        return dp[idx] = false;
+14    }
+15    bool wordBreak(string s, vector<string>& wordDict) {
+16        unordered_set<string> st(wordDict.begin(), wordDict.end());
+17        n = s.size();
+18        vector<int> dp(n + 1, -1);
+19       
+20        return solve(s, st, 0, dp);
+21    }
+22};
