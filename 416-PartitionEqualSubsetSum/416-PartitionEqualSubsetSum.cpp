a6ef23ff3e7ef96c3e1076009552dad37f2c1abf
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-15 22:38:18 +0530

    File updated at 7/15/2026, 10:38:19 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
@@ -1,21 +1,21 @@
-// Last updated: 3/18/2026, 2:27:38 PM
-class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum % 2 != 0) return false;
-        
-        int target = sum / 2;
-
-        vector<int> dp(target + 1, false);
-        dp[0] = true;
-
-        for(int num : nums) {
-            for(int j = target; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
-            }
-        }
-        return dp[target];
-    }
-};
+// Last updated: 7/15/2026, 10:38:19 PM
+1class Solution {
+2public:
+3    int n;
+4        bool solve(vector<int>& nums, int idx, int sum, vector<vector<int>>& dp) {
+5            if(sum == 0) return true;
+6            if(sum < 0) return false;
+7            if(idx == n) return false;
+8            if(dp[idx][sum] != -1) return dp[idx][sum];
+9
+10            return dp[idx][sum] = solve(nums, idx + 1, sum - nums[idx], dp) || solve(nums, idx + 1, sum, dp);
+11        }
+12    bool canPartition(vector<int>& nums) {
+13        n = nums.size();
+14        int sum = accumulate(nums.begin(), nums.end(), 0);
+15        if(sum % 2 != 0) return false;
+16        vector<vector<int>> dp(n + 1, vector<int>(sum / 2 + 1, -1));
+17
+18        return solve(nums, 0, sum / 2, dp);
+19    }
+20};
