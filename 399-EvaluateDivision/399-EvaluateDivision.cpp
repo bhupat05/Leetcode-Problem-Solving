5d9622abcd2b3a329e256b9d4f833f2a227d7909
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-17 15:09:19 +0530

    File updated at 7/17/2026, 3:09:20 PM

--- 399-EvaluateDivision/399-EvaluateDivision.cpp
+++ 399-EvaluateDivision/399-EvaluateDivision.cpp
@@ -1,47 +1,43 @@
-// Last updated: 3/18/2026, 2:27:42 PM
-class Solution {
-public:
-    unordered_map<string, vector<pair<string, double>>> graph;
-
-    double dfs(string src, string dest, unordered_set<string>& visited) {
-        if(!graph.count(src)) return -1.0;
-
-        if(src == dest) return 1.0;
-
-        visited.insert(src);
-
-        for(auto &edge : graph[src]) {
-            string next = edge.first;
-            double value = edge.second;
-
-            if(visited.count(next)) continue;
-
-            double result = dfs(next, dest, visited);
-            if(result != -1.0) {
-                return value * result;
-            }
-        } 
-        return -1;
-    }
-
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        
-        for(int i = 0; i < equations.size(); i++) {
-            string a = equations[i][0];
-            string b = equations[i][1];
-            double v = values[i];
-
-            graph[a].push_back({b, v});
-            graph[b].push_back({a, 1.0 / v});
-        }
-
-        vector<double> ans;
-
-        for(auto &q : queries) {
-            unordered_set<string> visited;
-            ans.push_back(dfs(q[0], q[1], visited));
-        }
-
-        return ans;
-    }
-};
+// Last updated: 7/17/2026, 3:09:20 PM
+1class Solution {
+2public:
+3    double dfs( unordered_map<string, vector<pair<string, double>>>& mp, unordered_set<string>& st, string src, string dest) {
+4        if(src == dest) return 1.0;
+5
+6        st.insert(src);
+7
+8        for(auto v : mp[src]) {
+9            if(st.count(v.first)) continue;
+10
+11            double ans = dfs(mp, st, v.first, dest);
+12
+13            if(ans != -1) return ans * v.second;
+14        }
+15        return -1;
+16    }
+17    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+18        int n = values.size();
+19        unordered_map<string, vector<pair<string, double>>> mp;
+20
+21        for(int i = 0; i < n; i++) {
+22            string a = equations[i][0];
+23            string b = equations[i][1];
+24            mp[a].push_back({b, values[i]});
+25            mp[b].push_back({a, 1.0 / values[i]});
+26        }
+27        vector<double> ans;
+28
+29        for(auto it : queries) {
+30            string a = it[0];
+31            string b = it[1];
+32
+33            if(!mp.count(a) || !mp.count(b)) {
+34                ans.push_back(-1.0);
+35            } else {
+36                unordered_set<string> st;
+37                ans.push_back(dfs(mp, st, a, b));
+38            }
+39        }
+40        return ans;
+41    }
+42};
