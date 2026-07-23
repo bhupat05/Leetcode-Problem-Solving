2e7a895ed3c918914c82314aa2d816dc947e9890
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-23 09:32:22 +0530

    File updated at 7/23/2026, 9:32:22 AM

--- 347-TopKFrequentElements/347-TopKFrequentElements.cpp
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -1,29 +1,32 @@
-// Last updated: 3/18/2026, 2:27:56 PM
-class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
-        priority_queue<pair<int, int>> pq;
-        unordered_map<int, int> mp;
-
-        for(int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-        }
-
-        for(auto it : mp) {
-            int a = it.first;
-            int b = it.second;
-
-            pq.push({b, a});
-        }
-
-        vector<int> r;
-
-        while(k--) {
-            int a = pq.top().second;
-            pq.pop();
-            r.push_back(a);
-        } 
-        return r;
-    }
-};
+// Last updated: 7/23/2026, 9:32:22 AM
+1class Solution {
+2public:
+3    vector<int> topKFrequent(vector<int>& nums, int k) {
+4
+5        unordered_map<int, int> freq;
+6
+7        for (int num : nums) {
+8            freq[num]++;
+9        }
+10
+11        vector<vector<int>> bucket(nums.size() + 1);
+12
+13        for (auto &p : freq) {
+14            bucket[p.second].push_back(p.first);
+15        }
+16
+17        vector<int> ans;
+18
+19        for (int i = nums.size(); i >= 1 && ans.size() < k; i--) {
+20
+21            for (int num : bucket[i]) {
+22                ans.push_back(num);
+23
+24                if (ans.size() == k)
+25                    return ans;
+26            }
+27        }
+28
+29        return ans;
+30    }
+31};
