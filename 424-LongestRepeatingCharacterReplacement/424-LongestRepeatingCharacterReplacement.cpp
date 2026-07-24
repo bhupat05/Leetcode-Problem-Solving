31825d0daa52d1020a385db2b7abdd48bb02bba0
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-24 18:04:59 +0530

    File updated at 7/24/2026, 6:04:58 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,29 +1,25 @@
-// Last updated: 7/24/2026, 10:15:40 AM
+// Last updated: 7/24/2026, 6:04:58 PM
 1class Solution {
 2public:
 3    int characterReplacement(string s, int k) {
-4        int n = s.length();
-5
-6        int l = 0;
-7        int r = 0;
-8        int maxlen = 0, maxf = 0;
-9        vector<int>count(26, 0);
-10
-11        while(r < n){
-12            count[s[r] - 'A']++;
-13            maxf = max(maxf, count[s[r] - 'A']);
-14
-15            while((r - l + 1) - maxf > k){
-16                count[s[l] - 'A']--;
-17                l++;
-18            }
-19
-20            maxlen = max(maxlen, r - l + 1);
-21            r++;
-22        }
-23
-24        return maxlen;
-25
-26
-27    }
-28};
+4        int n = s.size();
+5        int ans = 0;
+6        int maxi = 0;
+7        int l = 0;
+8        vector<int> v(26, 0);
+9
+10        for (int i = 0; i < n; i++) {
+11            v[s[i] - 'A']++;
+12            maxi = max(maxi, v[s[i] - 'A']);
+13
+14            while ((i - l + 1) - maxi > k) {
+15                v[s[l] - 'A']--;
+16                l++;
+17            }
+18
+19            ans = max(ans, i - l + 1);
+20        }
+21
+22        return ans;
+23    }
+24};
