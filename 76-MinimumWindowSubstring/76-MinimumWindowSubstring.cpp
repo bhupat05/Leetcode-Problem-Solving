30fdd4ae21c40b8ab492bc17e6d67eed58e15728
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-07-27 22:15:24 +0530

    File updated at 7/27/2026, 10:15:24 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
@@ -1,27 +1,41 @@
-// Last updated: 3/26/2026, 1:23:59 AM
+// Last updated: 7/27/2026, 10:15:24 PM
 1class Solution {
 2public:
-3    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-4        int i = 0;
-5        vector<vector<int>> result;
-6        int n = intervals.size();
+3    string minWindow(string s, string t) {
+4        int m = s.size();
+5        int n = t.size();
+6        if(n > m) return "";
 7
-8        while(i < n){
-9            if(intervals[i][1] < newInterval[0]){
-10                result.push_back(intervals[i]);
-11            }else if(intervals[i][0] > newInterval[1]){
-12                break;
-13            }else{
-14                newInterval[0] = min(newInterval[0], intervals[i][0]);
-15                newInterval[1] = max(newInterval[1], intervals[i][1]);
-16            }
-17            i++;
-18        }
-19        result.push_back(newInterval);
-20        while(i < n){
-21            result.push_back(intervals[i]);
-22            i++;
-23        }
-24        return result;
-25    }
-26};
+8        int start = -1;
+9        int l = 0;
+10        int mini = INT_MAX;
+11        int req = 0;
+12        vector<int> v(256, 0);
+13
+14        for(int i = 0; i < n; i++) {
+15            v[t[i]]++;
+16            req++;
+17        }
+18
+19        for(int i = 0; i < m; i++) {
+20            if(v[s[i]] > 0) {
+21                req--;
+22                
+23            }
+24            v[s[i]]--;
+25
+26            while(req == 0) {
+27                if(i - l + 1 < mini) {
+28                    mini = i - l + 1;
+29                    start = l;
+30                }
+31
+32                v[s[l]]++;
+33                if(v[s[l]] > 0) req++;
+34                l++;
+35            }
+36
+37        }
+38        return start == -1 ? "" : s.substr(start, mini);
+39    }
+40};
