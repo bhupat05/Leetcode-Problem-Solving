dc89c781cc105ba31fd5bc98437e6d7165671881
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-08-15 23:23:28 +0530

    File updated at 8/15/2026, 11:23:27 PM

--- 567-PermutationinString/567-PermutationinString.cpp
+++ 567-PermutationinString/567-PermutationinString.cpp
@@ -1,27 +1,41 @@
-// Last updated: 8/15/2026, 10:48:41 PM
+// Last updated: 8/15/2026, 11:23:27 PM
 1class Solution {
 2public:
-3    bool solve(vector<int> v, string s, int i, int j) {
-4        for(int k = i; k < j; k++) {
-5            v[s[k] - 'a']--;
-6        }
-7        for(int i = 0; i < 26; i++) {
-8            if(v[i] > 0) return false;
-9        }
-10
-11        return true;
-12    }
-13    bool checkInclusion(string s1, string s2) {
-14        int m = s1.size();
-15        vector<int> v(26, 0);
-16
-17        for(char c : s1) {
-18            v[c - 'a']++;
-19        }   
-20
-21        for(int i = 0; i + m <= s2.size(); i++) {
-22            if(solve(v, s2, i, i + m)) return true;
-23        }
-24        return false;
-25    }
-26};
+3    bool checkInclusion(string s1, string s2) {
+4        int m = s1.size();
+5        int n = s2.size();
+6
+7        if (m > n) return false;
+8
+9        vector<int> v(26, 0);
+10        int req = m;
+11
+12        for(char c : s1) {
+13            v[c - 'a']++;
+14        }
+15
+16        int l = 0;
+17
+18        for(int i = 0; i < n; i++) {
+19
+20            if(v[s2[i] - 'a'] > 0)
+21                req--;
+22
+23            v[s2[i] - 'a']--;
+24
+25            if(i - l + 1 > m) {
+26                v[s2[l] - 'a']++;
+27
+28                if(v[s2[l] - 'a'] > 0)
+29                    req++;
+30
+31                l++;
+32            }
+33
+34            if(req == 0)
+35                return true;
+36        }
+37
+38        return false;
+39    }
+40};
