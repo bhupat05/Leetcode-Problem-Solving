72bc3d0154f9324c4e940b9c4bf8a4c647b0079f
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-08-15 19:05:38 +0530

    File updated at 8/15/2026, 7:05:37 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.cpp
@@ -1,26 +1,24 @@
-// Last updated: 3/18/2026, 2:28:33 PM
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-
-        deque<int> deq;
-        vector<int> result;
-
-        for(int i = 0; i < n; i++){
-            while(!deq.empty() && deq.front() <= i - k){
-                deq.pop_front();
-            }
-
-            while(!deq.empty() && nums[i] >= nums[deq.back()]){
-                deq.pop_back();
-            }
-            deq.push_back(i);
-
-            if(i >= k - 1){
-                result.push_back(nums[deq.front()]);
-            }
-        }
-        return result;
-    }
-};
+// Last updated: 8/15/2026, 7:05:37 PM
+1class Solution {
+2public:
+3    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+4        int n = nums.size();
+5
+6        deque<int> deq;
+7        vector<int> result;
+8
+9        for(int i = 0; i < n; i++){
+10            if(i >= k && deq.front() == nums[i - k]) deq.pop_front();
+11
+12            while(!deq.empty() && nums[i] > deq.back()){
+13                deq.pop_back();
+14            }
+15            deq.push_back(nums[i]);
+16
+17            if(i >= k - 1){
+18                result.push_back(deq.front());
+19            }
+20        }
+21        return result;
+22    }
+23};
