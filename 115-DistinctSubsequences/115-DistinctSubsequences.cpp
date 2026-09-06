dc171f0b86a96ced0a041804c34099551edbc265
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-06 12:02:33 +0530

    File updated at 9/6/2026, 12:02:33 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.cpp
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -1,24 +1,21 @@
-// Last updated: 3/18/2026, 2:30:32 PM
-class Solution {
-public:
-    int numDistinct(string s, string t) {
-        int m = s.size(), n = t.size();
-        vector<vector<double>> dp(m + 1, vector<double>(n + 1, 0));
-
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        return (int)dp[m][n]; 
-    }
-};
+// Last updated: 9/6/2026, 12:02:33 PM
+1class Solution {
+2public:
+3    int m, n;
+4    int solve(string s, string t, int i, int j, vector<vector<int>>& dp) {
+5        if(j >= n) return 1;
+6        if(i >= m) return 0;
+7        if(dp[i][j] != -1) return dp[i][j];
+8
+9        if(s[i] == t[j]) {
+10            return dp[i][j] = solve(s, t, i + 1, j + 1, dp) + solve(s, t, i + 1, j, dp);
+11        }
+12        return dp[i][j] = solve(s, t, i + 1, j, dp);
+13    }
+14    int numDistinct(string s, string t) {
+15        m = s.size();
+16        n = t.size();
+17        vector<vector<int>> dp(m, vector<int> (n, -1));
+18        return solve(s, t, 0, 0, dp);
+19    }
+20};
