2e8f5a9f66922afb8753abed33d9aece7b5fb65b
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-07 15:27:47 +0530

    File updated at 9/7/2026, 3:27:47 PM

--- 940-DistinctSubsequencesII/940-DistinctSubsequencesII.cpp
+++ 940-DistinctSubsequencesII/940-DistinctSubsequencesII.cpp
@@ -1,33 +1,26 @@
-// Last updated: 9/7/2026, 12:09:36 PM
+// Last updated: 9/7/2026, 3:27:47 PM
 1class Solution {
 2public:
 3    long long mod = 1e9 + 7;
-4    int n;
-5    
-6    long long solve(int i, vector<int>& pre, vector<long long>& dp) {
-7        if(i == 0) return 1;
-8        if(i < 0) return 0;
-9        if(dp[i] != -1) return dp[i];
-10
-11        if(pre[i] != -1) {
-12            return dp[i] = (solve(i - 1, pre, dp) * 2 - solve(pre[i] - 1, pre, dp) + mod) % mod;
-13        } else {
-14            return dp[i] = (2 * solve(i-1, pre, dp) + 1) % mod;
-15        }
-16
-17
-18    }
-19    int distinctSubseqII(string s) {
-20        n = s.size();
-21        vector<int> pre(n, -1);
-22        vector<int> v(26, -1);
-23        vector<long long> dp(n, -1);
-24        for(int i = 0; i < n; i++) {
-25            if(v[s[i] - 'a'] != -1) {
-26                pre[i] = v[s[i] - 'a'];
-27            }
-28            v[s[i] - 'a'] = i;
-29        }
-30        return solve(n - 1, pre, dp);
-31    }
-32};
+4    int distinctSubseqII(string s) {
+5        int n = s.size();
+6        vector<int> pre(n, -1);
+7        vector<int> v(26, -1);
+8        vector<long long> dp(n, -1);
+9        for(int i = 0; i < n; i++) {
+10            if(v[s[i] - 'a'] != -1) {
+11                pre[i] = v[s[i] - 'a'];
+12            }
+13            v[s[i] - 'a'] = i;
+14        }
+15        dp[0] = 1;
+16        for(int i = 1; i < n; i++) {
+17            if(pre[i] != -1) {
+18                dp[i] = (2 * dp[i - 1] - (pre[i] == 0 ? 0 : dp[pre[i] - 1]) + mod) % mod;
+19            } else {
+20                dp[i] = (2 * dp[i - 1] + 1) % mod;
+21            }
+22        }
+23        return dp[n - 1];
+24    }
+25};
