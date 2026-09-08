ba97eab149a619ac59fc719f5862dfbab13ad247
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-08 18:54:14 +0530

    File updated at 9/8/2026, 6:54:14 PM

--- 767-ReorganizeString/767-ReorganizeString.cpp
+++ 767-ReorganizeString/767-ReorganizeString.cpp
@@ -1,44 +1,42 @@
-// Last updated: 9/8/2026, 3:08:48 PM
+// Last updated: 9/8/2026, 6:54:14 PM
 1class Solution {
 2public:
 3    string reorganizeString(string s) {
 4        int n = s.size();
-5        sort(s.begin(), s.end());
-6        vector<pair<int, char>> arr;
-7        int cnt = 1;
-8        char curr = s[0];
-9
-10        for(int i = 1; i < n; i++) {
-11            if(s[i] != s[i - 1]) {
-12                arr.push_back({cnt, curr});
-13                curr = s[i];
-14                cnt = 1;
-15            } else {
-16                cnt++;
-17            }
-18        }
-19        arr.push_back({cnt, curr});
-20
-21        sort(arr.rbegin(), arr.rend());
-22
-23        int l = 0;
-24        vector<char> ans(n, '#');
-25
-26        for(auto[cnt, c] : arr) {
-27            while(cnt--) {
-28                ans[l] = c;
-29                l += 2;
-30                if(l >= n) {
-31                    l = 1;
-32                }
+5        vector<int> fre(26, 0);
+6        for(char c : s) {
+7            fre[c - 'a']++;
+8        }
+9        int mx = 0;
+10        char c;
+11
+12        for(int i = 0; i < 26; i++) {
+13            if(fre[i] > mx) {
+14                mx = fre[i];
+15                c = i + 'a';
+16            }
+17        }
+18        if(mx > (n + 1) / 2) return "";
+19        vector<int> ans(n, '#');
+20        int l = 0;
+21        while(mx--) {
+22            ans[l] = c;
+23            l += 2;
+24            if(l >= n) l = 1;
+25        }
+26        for(int i = 0; i < 26; i++) {
+27            if((i + 'a') == c) continue;
+28
+29            while(fre[i]--) {
+30                ans[l] = i + 'a';
+31                l += 2;
+32                if(l >= n) l = 1;
 33            }
 34        }
-35        string r = string(1, ans[0]);
-36        for(int i = 1; i < n; i++) {
-37            if(ans[i] == r[i - 1] || ans[i] == '#') return "";
-38            r += string(1, ans[i]);
-39        }
-40        return r;
-41
-42    }
-43};
+35        string r = "";
+36        for(char c : ans) {
+37            r += string(1, c);
+38        }
+39        return r;
+40    }
+41};
