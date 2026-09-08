ddf782c8628dff684cbfef766c1ab39fa3f196c9
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-08 20:10:28 +0530

    File updated at 9/8/2026, 8:10:28 PM

--- 203-RemoveLinkedListElements/203-RemoveLinkedListElements.cpp
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.cpp
@@ -1,30 +1,30 @@
-// Last updated: 3/18/2026, 2:29:12 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* removeElements(ListNode* head, int val) {
-        while (head != nullptr && head->val == val) {
-            head = head->next;
-        }
-
-        ListNode* curr = head;
-        while (curr != nullptr && curr->next != nullptr) {
-            if (curr->next->val == val) {
-                curr->next = curr->next->next; 
-            } else {
-                curr = curr->next; 
-            }
-        }
-
-        return head;
-    }
-};
+// Last updated: 9/8/2026, 8:10:28 PM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* removeElements(ListNode* head, int val) {
+14        while (head != nullptr && head->val == val) {
+15            head = head->next;
+16        }
+17
+18        ListNode* curr = head;
+19        while (curr != nullptr && curr->next != nullptr) {
+20            if (curr->next->val == val) {
+21                curr->next = curr->next->next; 
+22            } else {
+23                curr = curr->next; 
+24            }
+25        }
+26
+27        return head;
+28    }
+29};
