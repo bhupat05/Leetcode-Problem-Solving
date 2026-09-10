1f9ace2056864e4c1c4b2c7622e6f09c2389a67c
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-10 11:41:18 +0530

    File updated at 9/10/2026, 11:41:19 AM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.cpp
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.cpp
@@ -1,41 +1,40 @@
-// Last updated: 9/10/2026, 11:36:51 AM
+// Last updated: 9/10/2026, 11:41:19 AM
 1class Solution {
 2public:
 3    int totalFruit(vector<int>& fruits) {
-4        int a = -1, b= -1;
-5        int l = 0, ans = 0;
-6        int cnt = 0;
+4        int a = -1, b = -1;   
+5        int left = 0;
+6        int ans = 0;
 7
-8        for(int i = 0; i < fruits.size(); i++) {
-9            if(cnt == 0) {
+8        for (int i = 0; i < fruits.size(); i++) {
+9            if (a == -1) {
 10                a = i;
-11                cnt = 1;
-12            } else if(cnt == 1) {
-13                if(fruits[i] == fruits[a]) {
-14                    a = i;
-15                    cnt = 1;
-16                } else {
-17                    b = i;
-18                    cnt = 2;
-19                }
-20            } else {
-21                if(fruits[i] == fruits[a]) {
-22                    a = i;
-23                } else if(fruits[i] == fruits[b]) {
-24                    b = i;
-25                } else {
-26                    int old = min(a, b);
-27                    l = old + 1;
-28                    if(a < b) {
-29                        a = i;
-30                    } else {
-31                        b = i;
-32                    }
-33                }
-34            }
-35
-36            ans = max(ans, i - l + 1);
-37        }
-38        return ans;
-39    }
-40};
+11            }
+12            else if (fruits[i] == fruits[a]) {
+13                a = i;
+14            }
+15            else if (b == -1) {
+16                b = i;
+17            }
+18
+19            else if (fruits[i] == fruits[b]) {
+20                b = i;
+21            }
+22
+23            else {
+24                left = min(a, b) + 1;
+25
+26                if (a < b)
+27                    a = i;
+28                else
+29                    b = i;
+30            }
+31
+32            ans = max(ans, i - left + 1);
+33        }
+34
+35        return ans;
+36    }
+37};
+38
+39
