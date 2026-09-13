031836e6c1aba622845920ffe0f3a78fcede4fc2
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-14 00:47:21 +0530

    File updated at 9/14/2026, 12:47:21 AM

--- 895-MaximumFrequencyStack/895-MaximumFrequencyStack.cpp
+++ 895-MaximumFrequencyStack/895-MaximumFrequencyStack.cpp
@@ -1,31 +1,33 @@
-// Last updated: 9/14/2026, 12:12:09 AM
+// Last updated: 9/14/2026, 12:47:21 AM
 1class FreqStack {
 2public:
 3    unordered_map<int,int> mp;
-4   priority_queue<pair<pair<int,int>, int>> pq;
-5    int cnt = 0;
-6    FreqStack() {
+4    vector<stack<int>> st;
+5    int mx = 0;
+6    FreqStack() : st(100005) {
 7        
 8    }
 9    
 10    void push(int val) {
 11        mp[val]++;
-12        pq.push({{mp[val], cnt++}, val});
-13    }
-14    
-15    int pop() {
-16        auto p = pq.top();
-17        pq.pop();
-18
-19        int val = p.second;
-20        mp[val]--;
-21        return val;
-22    }
-23};
-24
-25/**
-26 * Your FreqStack object will be instantiated and called as such:
-27 * FreqStack* obj = new FreqStack();
-28 * obj->push(val);
-29 * int param_2 = obj->pop();
-30 */
+12        mx = max(mx, mp[val]);
+13
+14        st[mp[val]].push(val);
+15
+16    }
+17    
+18    int pop() {
+19        int val = st[mx].top();
+20        st[mx].pop();
+21        mp[val]--;
+22        if(st[mx].empty()) mx--;
+23        return val;
+24    }
+25};
+26
+27/**
+28 * Your FreqStack object will be instantiated and called as such:
+29 * FreqStack* obj = new FreqStack();
+30 * obj->push(val);
+31 * int param_2 = obj->pop();
+32 */
