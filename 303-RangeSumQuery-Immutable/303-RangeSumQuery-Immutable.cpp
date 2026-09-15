37ad416c19facbd881a9204aff649e338954e087
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-16 02:36:01 +0530

    File updated at 9/16/2026, 2:36:01 AM

--- 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.cpp
+++ 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.cpp
@@ -1,22 +1,23 @@
-// Last updated: 9/16/2026, 2:33:38 AM
+// Last updated: 9/16/2026, 2:36:01 AM
 1class NumArray {
 2public:
-3    vector<int> nums;
-4    NumArray(vector<int>& nums) : nums(nums) {
-5        
-6    }
-7    
-8    int sumRange(int left, int right) {
-9        int sum = 0;
-10        for(int i = left; i <= right; i++) {
-11            sum += nums[i];
-12        }
-13        return sum;
-14    }
-15};
-16
-17/**
-18 * Your NumArray object will be instantiated and called as such:
-19 * NumArray* obj = new NumArray(nums);
-20 * int param_1 = obj->sumRange(left,right);
-21 */
+3    vector<int> pre;
+4    NumArray(vector<int>& nums) {
+5        int n = nums.size();
+6        pre.resize(n + 1, 0);
+7
+8        for(int i = 0; i < n; i++) {
+9            pre[i + 1] = pre[i] + nums[i];
+10        }
+11    }
+12    
+13    int sumRange(int left, int right) {
+14        return pre[right + 1] - pre[left];
+15    }
+16};
+17
+18/**
+19 * Your NumArray object will be instantiated and called as such:
+20 * NumArray* obj = new NumArray(nums);
+21 * int param_1 = obj->sumRange(left,right);
+22 */
