add8a686012eaa6711330243fac47ca0adc94b48
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-17 21:11:58 +0530

    File updated at 9/17/2026, 9:11:58 PM

--- 1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.cpp
+++ 1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum/1477-FindTwoNon-overlappingSub-arraysEachWithTargetSum.cpp
@@ -1,43 +1,25 @@
-// Last updated: 9/17/2026, 3:14:13 PM
+// Last updated: 9/17/2026, 9:11:58 PM
 1class Solution {
 2public:
 3    int minSumOfLengths(vector<int>& arr, int target) {
 4        int n = arr.size();
-5        vector<int> start(n, INT_MAX);
-6        vector<int> end(n, INT_MAX);
-7
-8        int l = 0;
-9        int sum = 0;
-10        for(int i = 0; i < n; i++) {
-11            sum += arr[i];
-12            while(l <= i && sum >= target) {
-13                if(sum == target) {
-14                    start[l] = i - l + 1;
-15                    end[i] = i - l + 1;
-16                }
-17                sum -= arr[l];
-18                l++;
-19            }
-20        }
-21        vector<int> pre(n);
-22        vector<int> suff(n);
-23
-24        pre[0] = end[0];
-25        for(int i = 1; i < n; i++) {
-26            pre[i] = min(end[i], pre[i - 1]);
-27        }
-28
-29        suff[n - 1] = start[n - 1];
-30        for(int i = n - 2; i >= 0; i--) {
-31            suff[i] = min(suff[i + 1], start[i]);
-32        }
-33        int ans = INT_MAX;
-34
-35        for(int i = 1; i < n - 1; i++) {
-36            if(pre[i - 1] != INT_MAX && suff[i] != INT_MAX) {
-37                ans = min(ans, pre[i - 1] + suff[i]);
-38            }
-39        }
-40        return ans == INT_MAX ? -1 : ans;
-41    }
-42};
+5        // [3, 2, 1, 4, 3] t = 3
+6        unordered_map<int, int> mp;
+7        mp[0] = -1;
+8        int sum = 0;
+9        int ans = n + 1;
+10        int mn = n;
+11        for(int i = 0; i < n; i++) {
+12            sum += arr[i];
+13            if(mp.count(sum - target)) {
+14                int l = mp[sum - target];
+15                int len = i - l;
+16                ans = min(ans, len + (l == -1 ? n : arr[l]));
+17                mn = min(mn, len);
+18            }
+19            arr[i] = mn;
+20            mp[sum] = i;
+21        }
+22        return ans == n + 1 ? -1 : ans;
+23    }
+24};
