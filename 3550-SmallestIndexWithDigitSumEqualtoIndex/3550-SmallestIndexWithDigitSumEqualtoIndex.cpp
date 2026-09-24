c3f3951678a2de1cd79d756175e8e1955a623516
bhupat05 <152694507+bhupat05@users.noreply.github.com>
2026-09-24 19:43:31 +0530

    File updated at 9/24/2026, 7:43:30 PM

--- 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.cpp
+++ 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.cpp
@@ -1,17 +1,20 @@
-// Last updated: 9/24/2026, 7:37:26 PM
+// Last updated: 9/24/2026, 7:43:30 PM
 1class Solution {
 2public:
 3    int smallestIndex(vector<int>& nums) {
-4        int n = nums.size();
-5        
-6        for(int i=0; i<n; i++){
-7            int sum = 0;
-8            while(nums[i]>0){
-9                sum += nums[i]%10;
-10                nums[i] = nums[i]/10;
+4        for (int i = 0; i < min((int)nums.size(), 28); i++) {
+5            int num = nums[i];
+6            int sum = 0;
+7
+8            while (num > 0) {
+9                sum += num % 10;
+10                num /= 10;
 11            }
-12            if(i == sum) return i;
-13        }
-14        return -1;
-15    }
-16};
+12
+13            if (sum == i)
+14                return i;
+15        }
+16
+17        return -1;
+18    }
+19};
